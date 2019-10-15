59613ff9fb5e3fcb397239ec9d7676851b115284
SASAO Takahiro <tsasao@gmail.com>
2019-10-15 16:35:09 +0900

    update images. add links.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
 var clients = [
-    { id: 100, pattern: '20-31-1', service: 'ダンドリペイメント', cost: '3.85%', limit: '' },
-    { id: 204, pattern: '31-10-2', service: 'ラグレス', cost: '3.85%', limit: '4' }
+    { id: 100, pattern: '20-31-1', mode: 'dandori', service: 'ダンドリペイメント', mail: 'd-p@invest-d.com', link: 'https://www.dandoli-payment.com', form_1: 'https://form.run/@dandolipayment-1-1', form_2: 'https://form.run/@dandolipayment-1-2', cost: '3.85%', limit: 'なし' },
+    { id: 204, pattern: '31-10-2', mode: 'lagless', service: 'ラグレス', mail: 'lagless@invest-d.com', link: 'https://www.lag-less.com', form_1: 'https://form.run/@dandolipayment-1-1', form_2: 'https://form.run/@dandolipayment-1-2', cost: '3.85%', limit: '年4回まで' },
+    { id: 300, pattern: '31-10-2', mode: 'renove', service: 'リノベ不動産Ｐａｙｍｅｎｔ', mail: 'lagless@invest-d.com', link: 'https://www.lag-less.com', form_1: 'https://form.run/@dandolipayment-1-1', form_2: 'https://form.run/@dandolipayment-1-2', cost: '3.85%', limit: '年4回まで' }
 ];
 
 $(function() {
     var query = location.search;
+    if(location.protocol == 'file:') { query = 'c=300' }
     var param = { };
     var pairs = query.replace(/^\?/, '').split(/&/);
     for(var i=0 ; i<pairs.length ; i++) {
@@ -22,5 +24,10 @@ $(function() {
     $('.patterns').hide();
     $('.pattern-'+client.pattern).show();
     $('.cost').text(client.cost);
-    $('.limit').text(client.limit || '無し');
+    $('.limit').text(client.limit);
+    $('.cond-'+client.mode).show();
+    $('.form_1').attr('href', client.form_1);
+    $('.form_2').attr('href', client.form_2);
+    $('.link').attr('href', client.link);
+    $('.mail').text(client.mail).attr('href', 'mailto:'+client.mail);
 });
