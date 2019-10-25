451e9bf46b4152753fef5f8e057cb34806c9a2d6
SASAO Takahiro <tsasao@gmail.com>
2019-10-25 18:03:05 +0900

    updated.

---
+++ functions/index.js
@@ -0,0 +1,99 @@
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+
+const conv = json => json.records.map(r => {
+    let record = {};
+    Object.keys(r).forEach(k => {
+        record[k] = r[k].value;
+    });
+    return record;
+});
+
+const except = (obj, keys) => {
+    let r = { };
+    for(k in obj) {
+        if(!keys.includes(k)) {
+            r[k] = obj[k];
+        }
+    }
+    return r;
+}
+
+const pattern_fields = [
+    'pattern',
+    'closing', 'deadline', 'early'
+].join(',');
+
+// 20パターン*12ヶ月 = 240くらいを想定
+const fetch_pattern = fetch("https://investdesign.cybozu.com/k/v1/records.json?"+querystring.stringify({
+    app: 95,
+    fields: pattern_fields,
+    query: 'deadline >= TODAY() and deadline <= FROM_TODAY(12, MONTHS) order by deadline asc limit 500',
+}), {
+    headers: { 'X-Cybozu-API-Token': '6Nfwr2ySlXxDBzFG8EV3u3nsHVgdr8tjLluN5JW3' },
+})
+.then(res => {
+    if(res.status == 200) {
+        return res.json();
+    } else {
+        res.text().then(text => { throw new Error(text) });
+    }
+})
+.then(conv)
+.then(records => {
+    let by_pattern = { };
+    records.forEach(r => {
+        if(!by_pattern[r.pattern]) {
+            by_pattern[r.pattern] = [ ];
+        }
+        by_pattern[r.pattern].push(except(r, [ 'pattern' ]));
+    });
+    return by_pattern;
+});
+
+const fields = [
+    'id', 'service', 'pattern', 'cost', 'limit',
+    'form_1', 'form_2', 'link', 'mail',
+    'closing', 'deadline', 'early', 'original', 'lag', 'effect'
+].join(',');
+
+const fetch_service = fetch("https://investdesign.cybozu.com/k/v1/records.json?app="+process.env['KINTONE_APP_ID']+"&fields="+fields, {
+    headers: { 'X-Cybozu-API-Token': process.env['KINTONE_TOKEN'] },
+})
+.then(res => {
+    if(res.status == 200) {
+        return res.json();
+    } else {
+        res.text().then(text => { throw new Error(text) });
+    }
+})
+.then(conv)
+.then(records => {
+    let by_id = { };
+    records.forEach(r => {
+        by_id[r.id] = except(r, [ 'id' ]);
+    });
+    return by_id;
+});
+
+exports.helloWorld = functions.https.onRequest(async (req, res) => {
+    const json = await Promise.all([ fetch_pattern, fetch_service ])
+    .then(result => {
+        pattern = result[0];
+        service = result[1];
+        for(id in service) {
+            service[id].schedule = pattern[service[id].pattern] || [ ];
+        }
+        return JSON.stringify(service, true, "  ");
+    });
+    
+    const bucket = admin.storage().bucket();
+    const file = bucket.file('data.json');
+    await file.save(json);
+    await file.setMetadata({ contentType: 'application/json' });
+    res.status(200).send('complete');
+});
--- public/app.js
+++ public/app.js
@@ -1,33 +1,106 @@
-var clients = [
-    { id: 100, pattern: '20-31-1', mode: 'dandori', service: 'ダンドリペイメント', mail: 'd-p@invest-d.com', link: 'https://www.dandoli-payment.com', form_1: 'https://form.run/@dandolipayment-1-1', form_2: 'https://form.run/@dandolipayment-1-2', cost: '3.85%', limit: 'なし' },
-    { id: 204, pattern: '31-10-2', mode: 'lagless', service: 'ラグレス', mail: 'lagless@invest-d.com', link: 'https://www.lag-less.com', form_1: 'https://form.run/@dandolipayment-1-1', form_2: 'https://form.run/@dandolipayment-1-2', cost: '3.85%', limit: '年4回まで' },
-    { id: 300, pattern: '31-10-2', mode: 'renove', service: 'リノベ不動産Ｐａｙｍｅｎｔ', mail: 'lagless@invest-d.com', link: 'https://www.lag-less.com', form_1: 'https://form.run/@dandolipayment-1-1', form_2: 'https://form.run/@dandolipayment-1-2', cost: '3.85%', limit: '年4回まで' }
-];
-
 $(function() {
     var query = location.search;
-    if(location.protocol == 'file:') { query = 'c=300' }
     var param = { };
     var pairs = query.replace(/^\?/, '').split(/&/);
     for(var i=0 ; i<pairs.length ; i++) {
         var kv = pairs[i].split(/=/, 2);
         param[kv[0]] = kv[1];
     }
-    var client = clients.filter(function(c) { return c.id==param.c; })[0];
-    if(!client) {
-        $('#error').text('不正なパラメータです.').show();
+
+    if(location.protocol == 'file:') {
+        show({
+            "cost": "3.85%",
+            "original": "翌月末",
+            "mail": "d-p@invest-d.com",
+            "form_2": "https://form.run/@dandolipayment-1-2",
+            "link": "https://www.dandoli-payment.com",
+            "pattern": "",
+            "form_1": "https://form.run/@dandolipayment-1-1",
+            "closing": "20日",
+            "lag": "40日",
+            "service": "ラグレス",//"ダンドリペイメント",
+            "effect": "30日",
+            "limit": "なし",
+            "id": "100",
+            "deadline": "24日",
+            "early": "当月末",
+            "schedule": [
+                {
+                    "closing": "2019-09-20",
+                    "deadline": "2019-09-22",
+                    "early": "2019-09-30"
+                },
+                {
+                    "closing": "2019-10-20",
+                    "deadline": "2019-10-22",
+                    "early": "2019-10-31"
+                },
+                {
+                    "closing": "2019-11-20",
+                    "deadline": "2019-11-22",
+                    "early": "2019-11-30"
+                },
+                {
+                    "closing": "2019-12-20",
+                    "deadline": "2019-12-22",
+                    "early": "2019-12-31"
+                }
+            ]
+        }, param);
+        $('#content').show();
         return;
     }
+    
+    var target = "https://firebasestorage.googleapis.com/v0/b/lagless.appspot.com/o/data.json?alt=media";
+    $.ajax({
+        url: target
+    }).then(function(resp) {
+        if(resp[param.c]) {
+            show(resp[param.c], param);
+            $('#content').show();
+        } else {
+            $('#error').text('不正なパラメータです.').show();
+        }
+    });
+});
 
+var format_date = function(str) {
+    var t = new Date(str);
+    var wod = [ '日', '月', '火', '水', '木', '金', '土' ];
+    return t.getFullYear()+'年'+(t.getMonth()+1)+'月'+t.getDate()+'日 ('+wod[t.getDay()]+')';
+};
+
+var show = function(client, param) {
     $('.service').text(client.service);
-    $('.pattern').text(client.pattern);
-    $('.patterns').hide();
-    $('.pattern-'+client.pattern).show();
+    var mode = { 'ラグレス': 'lagless', 'ダンドリペイメント': 'dandori', 'リノベ不動産Payment': 'renove' }[client.service];
+    
     $('.cost').text(client.cost);
     $('.limit').text(client.limit);
-    $('.cond-'+client.mode).show();
+    $('.cond-'+mode).show();
     $('.form_1').attr('href', client.form_1);
     $('.form_2').attr('href', client.form_2);
     $('.link').attr('href', client.link);
     $('.mail').text(client.mail).attr('href', 'mailto:'+client.mail);
-});
+    
+    $('.closing').text(client.closing);
+    $('.deadline').text(client.deadline);
+    $('.early').text(client.early);
+    $('.original').text(client.original);
+    $('.closing').text(client.closing);
+    $('.lag').text(client.lag);
+    $('.effect').text(client.effect);
+
+    var now = new Date();
+    var schedule = client.schedule.find(function(s) {
+        return now.getTime() <= (new Date(s.deadline)).getTime();
+    });
+    if(schedule) {
+        $('.schedule_closing').text(format_date(schedule.closing));
+        $('.schedule_early').text(format_date(schedule.early));
+        $('.schedule_deadline').text(format_date(schedule.deadline));
+    }
+    
+    if(param.f) {
+        $('.first').show();
+    }
+};
