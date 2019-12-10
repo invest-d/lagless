b4f2e4fa6dbc3e1725f06713e9860bf6579e4760
SASAO Takahiro <tsasao@gmail.com>
2019-12-10 22:30:13 +0900

    fix bug.

--- functions/index.js
+++ functions/index.js
@@ -29,31 +29,33 @@ const pattern_fields = [
 ].join(',');
 
 // 20パターン*12ヶ月 = 240くらいを想定
-const fetch_pattern = fetch("https://investdesign.cybozu.com/k/v1/records.json?"+querystring.stringify({
-    app: parseInt(process.env['KINTONE_PATTERN_APP_ID']),
-    fields: pattern_fields,
-    query: 'deadline >= TODAY() and deadline <= FROM_TODAY(12, MONTHS) order by deadline asc limit 500',
-}), {
-    headers: { 'X-Cybozu-API-Token': process.env['KINTONE_PATTERN_TOKEN'] },
-})
-.then(res => {
-    if(res.status == 200) {
-        return res.json();
-    } else {
-        res.text().then(text => { throw new Error(text) });
-    }
-})
-.then(conv)
-.then(records => {
-    let by_pattern = { };
-    records.forEach(r => {
-        if(!by_pattern[r.pattern]) {
-            by_pattern[r.pattern] = [ ];
+const fetch_pattern = function() {
+    return fetch("https://investdesign.cybozu.com/k/v1/records.json?"+querystring.stringify({
+        app: parseInt(process.env['KINTONE_PATTERN_APP_ID']),
+        fields: pattern_fields,
+        query: 'deadline >= TODAY() and deadline <= FROM_TODAY(12, MONTHS) order by deadline asc limit 500',
+    }), {
+        headers: { 'X-Cybozu-API-Token': process.env['KINTONE_PATTERN_TOKEN'] },
+    })
+    .then(res => {
+        if(res.status == 200) {
+            return res.json();
+        } else {
+            res.text().then(text => { throw new Error(text) });
         }
-        by_pattern[r.pattern].push(except(r, [ 'pattern' ]));
+    })
+    .then(conv)
+    .then(records => {
+        let by_pattern = { };
+        records.forEach(r => {
+            if(!by_pattern[r.pattern]) {
+                by_pattern[r.pattern] = [ ];
+            }
+            by_pattern[r.pattern].push(except(r, [ 'pattern' ]));
+        });
+        return by_pattern;
     });
-    return by_pattern;
-});
+};
 
 const fields = [
     'id', 'service', 'pattern', 'cost', 'limit',
@@ -61,32 +63,55 @@ const fields = [
     'closing', 'deadline', 'early', 'original', 'lag', 'effect'
 ].join(',');
 
-const fetch_service = fetch("https://investdesign.cybozu.com/k/v1/records.json?app="+process.env['KINTONE_BUILDER_APP_ID']+"&fields="+fields, {
-    headers: { 'X-Cybozu-API-Token': process.env['KINTONE_BUILDER_TOKEN'] },
-})
-.then(res => {
-    if(res.status == 200) {
-        return res.json();
-    } else {
-        res.text().then(text => { throw new Error(text) });
-    }
-})
-.then(conv)
-.then(records => {
-    let by_id = { };
-    records.forEach(r => {
-        by_id[r.id] = except(r, [ 'id' ]);
+const fetch_service = function() {
+    return fetch("https://investdesign.cybozu.com/k/v1/records.json?"+querystring.stringify({
+        app: process.env['KINTONE_BUILDER_APP_ID'],
+        fields: fields,
+    }), {
+        headers: { 'X-Cybozu-API-Token': process.env['KINTONE_BUILDER_TOKEN'] },
+    })
+    .then(res => {
+        if(res.status == 200) {
+            return res.json();
+        } else {
+            res.text().then(text => { throw new Error(text) });
+        }
+    })
+    .then(conv)
+    .then(records => {
+        let by_id = { };
+        records.forEach(r => {
+            by_id[r.id] = except(r, [ 'id' ]);
+        });
+        return by_id;
     });
-    return by_id;
-});
+};
 
 exports.helloWorld = functions.https.onRequest(async (req, res) => {
-    const json = await Promise.all([ fetch_pattern, fetch_service ])
+    res.set('Access-Control-Allow-Origin', 'https://investdesign.cybozu.com');
+    res.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if(req.method != 'POST') {
+        res.status(200).send('unexpected method');
+        return;
+    }
+    
+    let ids = [ ];
+    console.log('fetch from kintone...');
+    const json = await Promise.all([ fetch_pattern(), fetch_service() ])
     .then(result => {
         pattern = result[0];
         service = result[1];
+        console.log('got '+Object.keys(pattern).length+' patterns and '+Object.keys(service).length+' services.');
         for(id in service) {
+            if(pattern[service[id].pattern]) {
+                console.log('service.id='+id+', pattern='+service[id].pattern);
+            } else {
+                console.log('no schedule for pattern[id='+service[id].pattern+'].');
+            }
             service[id].schedule = pattern[service[id].pattern] || [ ];
+            ids.push(id);
         }
         return JSON.stringify(service, true, "  ");
     });
@@ -95,5 +120,8 @@ exports.helloWorld = functions.https.onRequest(async (req, res) => {
     const file = bucket.file('data.json');
     await file.save(json);
     await file.setMetadata({ contentType: 'application/json' });
-    res.status(200).send('complete');
+    res.set('Content-Type', 'application/json');
+    res.status(200).send(JSON.stringify({ success: true, service_ids: ids }));
+    
+    console.log('completed.');
 });
