084e8b0de8faf09271e644d2db0afeeec0eae63b
atsushianaaki <atushi_anaaki@yahoo.co.jp>
2020-04-04 17:43:09 +0900

    文字揃え

--- kintone_js/output_csv.js
+++ kintone_js/output_csv.js
@@ -11,7 +11,7 @@
 (function() {
     "use strict";
 
-    const APP_ID_APPLY = kintone.app.getId();
+    const APP_ID_APPLY              = kintone.app.getId();
     const fieldRecordId_APPLY       = 'レコード番号';
     const fieldBankCode_APPLY       = 'bankCode';
     const fieldBranchCode_APPLY     = 'branchCode';
