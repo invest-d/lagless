d44a43da20dd8687e63169cef66267357467c6a1
atsushianaaki <atushi_anaaki@yahoo.co.jp>
2020-04-08 17:45:12 +0900

    各種変数名をjudge -> examに変更

--- kintone_js/copy_credit_button.js
+++ kintone_js/copy_credit_button.js
@@ -20,11 +20,11 @@
 (function (){
     "use strict";
 
-    const APP_ID_JUDGE = 79;
+    const APP_ID_EXAM = 79;
     // フィールドコード
-    const customerCode_JUDGE = '取引企業管理No_審査対象企業';
-    const creditAmount_JUDGE = '付与与信枠_決定金額_標準と高額';
-    const judgedDay_JUDGE = '審査完了日';
+    const customerCode_EXAM = '取引企業管理No_審査対象企業';
+    const creditAmount_EXAM = '付与与信枠_決定金額_標準と高額';
+    const examinedDay_EXAM = '審査完了日';
 
     const APP_ID_KOMUTEN = 96;
     const recordNo_KOMUTEN = 'レコード番号';
@@ -65,18 +65,18 @@
 
         try {
             // 審査内容が最新のレコードを取得。判定基準は審査完了日フィールド。
-            const latest_judge_records = await getLatestJudgeRecords()
+            const latest_exam_records = await getLatestExamRecords()
             .catch((err) => {
                 console.log(err);
                 throw new Error('審査アプリからのレコード取得中にエラーが発生しました。');
             });
 
 
-            if (latest_judge_records.length === 0) {
+            if (latest_exam_records.length === 0) {
                 throw new Error('与信枠が入力済みの企業はありませんでした。\nどの工務店の付与与信枠も更新されていません。');
             }
 
-            const updated_count = await updateKomutenCredits(latest_judge_records)
+            const updated_count = await updateKomutenCredits(latest_exam_records)
             .catch((err) => {
                 console.log(err);
                 throw new Error('取得した与信枠を工務店マスタに更新している途中にエラーが発生しました。');
@@ -90,31 +90,31 @@
         };
     }
 
-    async function getLatestJudgeRecords() {
+    async function getLatestExamRecords() {
         console.log('審査アプリの中で審査完了日が最新のレコードを取引企業Noごとにすべて取得する。');
 
         // 全件取得
-        const body_judges = {
-            'app': APP_ID_JUDGE,
-            'fields': [customerCode_JUDGE, creditAmount_JUDGE, judgedDay_JUDGE],
-            'query': `${judgedDay_JUDGE} != \"\" and ${creditAmount_JUDGE} >= 0`, // ヤバい取引企業は与信枠ゼロにして対応することもあるので、ゼロ円のレコードも取得
+        const body_exams = {
+            'app': APP_ID_EXAM,
+            'fields': [customerCode_EXAM, creditAmount_EXAM, examinedDay_EXAM],
+            'query': `${examinedDay_EXAM} != \"\" and ${creditAmount_EXAM} >= 0`, // ヤバい取引企業は与信枠ゼロにして対応することもあるので、ゼロ円のレコードも取得
             'seek': true
         };
 
-        const all_judge = await kintoneRecord.getAllRecordsByQuery(body_judges);
+        const all_exam = await kintoneRecord.getAllRecordsByQuery(body_exams);
 
         // 重複なしの取引企業Noを取得
-        const unique_customer_codes = all_judge.records
-            .map((record) => record[customerCode_JUDGE]['value'])
+        const unique_customer_codes = all_exam.records
+            .map((record) => record[customerCode_EXAM]['value'])
             .filter((code, i, self) => self.indexOf(code) === i);
 
-        const latest_judges = unique_customer_codes.map((customer_code) => {
+        const latest_exams = unique_customer_codes.map((customer_code) => {
             // 取引企業Noごとにfilterして処理する
-            const target_judges = all_judge.records.filter(record => record[customerCode_JUDGE]['value'] === customer_code);
+            const target_exams = all_exam.records.filter(record => record[customerCode_EXAM]['value'] === customer_code);
 
             // 日付だけの配列を作って、配列の中の最新の日付を取得する
-            const judge_dates = target_judges.map(record => getComparableDate(record[judgedDay_JUDGE]['value']));
-            const latest_date = new Date(Math.max.apply(null, judge_dates));
+            const examined_dates = target_exams.map(record => getComparableDate(record[examinedDay_EXAM]['value']));
+            const latest_date = new Date(Math.max.apply(null, examined_dates));
 
             // kintone書式との一致を確認するためにyyyy-mm-ddにフォーマット
             const latest_yyyy_mm_dd = [
@@ -124,10 +124,10 @@
             ].join('-');
 
             // 審査レコードの中で、最新日付を持っているものを取得
-            return target_judges.find(record => record[judgedDay_JUDGE]['value'] === latest_yyyy_mm_dd);
+            return target_exams.find(record => record[examinedDay_EXAM]['value'] === latest_yyyy_mm_dd);
         });
 
-        return latest_judges;
+        return latest_exams;
     }
 
     // YYYY-MM-DDの書式のままでは日付の比較ができないので、比較可能な値に変換する
@@ -137,29 +137,29 @@
         return date.getTime();
     }
 
-    async function updateKomutenCredits(latest_judge_records) {
+    async function updateKomutenCredits(latest_exam_records) {
         console.log('工務店マスタからレコードIDと取引企業Noの一覧を取得する');
         // 付与与信枠を取得したくない工務店は除外する
-        const body_judges = {
+        const body_exams = {
             'app': APP_ID_KOMUTEN,
             'fields': [recordNo_KOMUTEN, customerCode_KOMUTEN],
             'query': `${fieldGetCreditNextTime_KOMUTEN} in (\"${statusGetCredit_KOMUTEN}\")`,
             'seek': true
         };
 
-        const komuten_info = await kintoneRecord.getAllRecordsByQuery(body_judges);
+        const komuten_info = await kintoneRecord.getAllRecordsByQuery(body_exams);
 
         // 各工務店レコードに、審査アプリから取得した与信枠をPUTするオブジェクトを作る。
         const put_records = komuten_info.records.map((komuten) => {
             // 審査レコードの中から、工務店レコードの取引企業Noフィールドと同じ取引企業Noのレコードを探してセット。
             // 工務店レコード1件に対して審査レコードは1件のみなのでfind（n件あるのは逆の場合）
             // 結果、匠和美健などの場合は一つの審査レコードの与信枠が複数の工務店レコードにセットされる。
-            const target_judge = latest_judge_records.find(record => record[customerCode_JUDGE]['value'] === komuten[customerCode_KOMUTEN]['value']);
+            const target_exam = latest_exam_records.find(record => record[customerCode_EXAM]['value'] === komuten[customerCode_KOMUTEN]['value']);
 
             // 1件も審査レコードがない場合はnullセット
-            const credit = (target_judge === undefined)
+            const credit = (target_exam === undefined)
                 ? null
-                : target_judge[creditAmount_JUDGE]['value'];
+                : target_exam[creditAmount_EXAM]['value'];
 
             return {
                 'id': komuten[recordNo_KOMUTEN]['value'],
@@ -175,7 +175,7 @@
 
         if (put_records.length === 0) {
             console.log('update targets are ');
-            console.log(latest_judge_records);
+            console.log(latest_exam_records);
             throw new Error('工務店マスタの中で、審査が行われている企業はありませんでした。\nどの工務店の付与与信枠も更新されていません。');
         }
 
