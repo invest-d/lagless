e7e227a06d51d312c81dd6dc89691a816f328bc1
atsushianaaki <atushi_anaaki@yahoo.co.jp>
2020-04-13 12:58:25 +0900

    再代入しない変数の宣言をletからconstに変更。リファラを判断して、開発版のフォームから申込があった場合は開発版のアプリへ、本番のフォームから申込があった場合は本番のフォームへとレコードを登録するようにした。

--- functions/send_apply.js
+++ functions/send_apply.js
@@ -5,35 +5,36 @@ const FormData = require('form-data');
 const axios = require('axios');
 
 exports.send_apply = functions.https.onRequest(async (req, res) => {
-    interceptor(req.headers.referer, res);
+    const env = new Environ(req.headers.referer).create();
+    interceptor(env, res);
     console.log('フォーム送信開始');
     if (req.method != 'POST') {
         res.status(405).send('Method Not Allowed');
         return;
     } else {
-        var sendObj = {};
-        sendObj.app = process.env.app_id_apply;
-        var record = {};
-        var file_key_result = [];
+        let sendObj = {};
+        sendObj.app = env.app_id;
+        let record = {};
+        let file_key_result = [];
 
         // 送信元のフォームのURLからuserパラメータを受け取り、ファイルアップロードがいくつあるかを確認する。
-        var user_query = req.headers.referer.match(/user=.+?($|&)/);
+        const user_query = req.headers.referer.match(/user=.+?($|&)/);
         console.log('user_query is');
         console.log(user_query);
-        var user_type = 'new';
-        if (user_query !== null && user_query.length > 0) {
-            user_type = user_query[0].replace('&', '').split('=')[1];
-        }
+        const user_type = (user_query !== null && user_query.length > 0)
+            ? user_query[0].replace('&', '').split('=')[1]
+            : 'new'; //userパラメータが無い場合は新規ユーザとする（フォームもパラメータが無い場合は新規として扱っている）
+
         console.log('user type is ' + user_type);
         // 2回目のフォームの場合、アップロードするファイルは請求書データのみなので1
-        var UPLOAD_REQUIRED = (user_type === "existing")
+        const UPLOAD_REQUIRED = (user_type === "existing")
             ? 1
             : 3;
-        console.log(UPLOAD_REQUIRED + 'uploads will be executed.');
+        console.log(UPLOAD_REQUIRED + ' files will be uploaded.');
 
         const busboy = new Busboy({ headers: req.headers });
         const allowMimeTypes = ['application/pdf', 'image/jpeg', 'image/png'];
-        var busboy_result = new Promise((resolve, reject) => {
+        const busboy_result = new Promise((resolve, reject) => {
             busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
                 if (mimetype == 'application/octet-stream') {
                     // 添付ファイルが未入力の場合（application/octet-stream）はスルー
@@ -48,11 +49,11 @@ exports.send_apply = functions.https.onRequest(async (req, res) => {
                     return;
                 } else {
                     const form = new FormData();
-                    var ext = String(mimetype).split('/')[1];
+                    const ext = String(mimetype).split('/')[1];
 
                     form.append('file', file, `${fieldname}.${ext}`);
-                    var headers = Object.assign(form.getHeaders(), {
-                        'X-Cybozu-API-Token': process.env.api_token_apply_files
+                    const headers = Object.assign(form.getHeaders(), {
+                        'X-Cybozu-API-Token': env.api_token_files
                     });
                     axios.post('https://investdesign.cybozu.com/k/v1/file.json', form, { headers })
                     .then(result => {
@@ -104,25 +105,22 @@ exports.send_apply = functions.https.onRequest(async (req, res) => {
 
             // データ加工
             // FAX番号を入力してもらってる場合はメールアドレスに加工して保存
-            var mail_or_fax = record["email-fax-input"]["value"];
-            console.log('email address input is '+ mail_or_fax);
-            if (mail_or_fax == 'fax') {
-                var fax_number = record.fax.value;
-                var emailed_fax = `lagless+fax-${fax_number}@invest-d.com`;
+            const mail_or_fax = record["email-fax-input"]["value"];
+            console.log('mail or fax is '+ mail_or_fax);
+            if (mail_or_fax === 'fax') {
+                const emailed_fax = `lagless+fax-${record.fax.value}@invest-d.com`;
                 record = Object.assign(record, {"mail": {"value": emailed_fax}});
                 delete record["fax"];
             }
 
 
-            // 預金種目を日本語に変換(初回申込みのみ)
+            // 預金種目を日本語に変換(新規ユーザのみ)
             if (user_type !== "existing") {
-                var deposit_type = record.deposit_Form.value;
-                var ja_deposit_type = "";
-                if (deposit_type == "ordinary") {
-                    ja_deposit_type = "普通";
-                } else {
-                    ja_deposit_type = "当座";
-                }
+                // どちらかを選んでいないとフォームからの送信は出来ない仕様
+                const ja_deposit_type = (record.deposit_Form.value == "ordinary")
+                    ? "普通"
+                    : "当座";
+
                 record = Object.assign(record, {"deposit_Form": {"value": ja_deposit_type}});
             }
 
@@ -137,24 +135,22 @@ exports.send_apply = functions.https.onRequest(async (req, res) => {
             console.log('get sendObj completed.');
             console.log(JSON.stringify(sendObj));
 
-            var request = require('request');
+            const request = require('request');
 
-            // LAGLESSアプリの工務店IDを元に工務店マスタのレコードを参照するため、両方のアプリのAPIトークンが必要
-            var API_APPLY = process.env.api_token_apply;
-            var API_KOMUTEN = process.env.api_token_komuten;
-            var API_TOKEN = API_APPLY + ',' + API_KOMUTEN;
+            // 申込みアプリの工務店IDを元に工務店マスタのレコードを参照するため、両方のアプリのAPIトークンが必要
+            const API_TOKEN = env.api_token_record + ',' + process.env.api_token_komuten;
 
-            var BASE_URL = 'https://investdesign.cybozu.com/k/v1/record.json';
-            var BASIC = 'Basic ' + API_TOKEN;
+            const BASE_URL = 'https://investdesign.cybozu.com/k/v1/record.json';
+            const BASIC = 'Basic ' + API_TOKEN;
 
-            var headers = {
-                'Host': 'investdesign.cybozu.com:' + process.env.app_id_apply,
+            const headers = {
+                'Host': 'investdesign.cybozu.com:' + env.app_id,
                 'X-Cybozu-API-Token': API_TOKEN,
                 'Authorization': BASIC,
                 'Content-Type': 'application/json',
             };
 
-            var options_postrecord = {
+            const options_postrecord = {
                 url: BASE_URL,
                 method: 'POST',
                 headers: headers,
@@ -165,9 +161,10 @@ exports.send_apply = functions.https.onRequest(async (req, res) => {
             request(options_postrecord, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     // 成功
-                    res.status(200).send(process.env.apply_success_page_url);
+                    res.status(200).send(env.success_redirect_to);
                 } else {
-                    console.error('response is ' + JSON.stringify(response));
+                    console.error('response is ' + response.statusCode + ': ' + response.body.code + ': ' + response.body.message);
+                    console.log('headers is ' + JSON.stringify(headers));
                     console.error('sendObj is ' + JSON.stringify(sendObj));
                     console.error('req.body is ' + JSON.stringify(req.body));
                     res.status(response.statusCode).redirect(req.headers.referer);
@@ -178,13 +175,50 @@ exports.send_apply = functions.https.onRequest(async (req, res) => {
 });
 
 // リクエストヘッダを確認し、hostが正規のフォームであればそれをCORSに設定
-function interceptor(referer, res){
-    host = extractHostDomain(referer);
-    console.log('requested from ' + String(host));
+function interceptor(env, res){
+    console.log('requested from ' + String(env.host));
 
-    if(isAllowedOrigin(host)){
-        // 開発版のフォームなら開発版のドメインを、本番のフォームなら本番のドメインを設定
-        res.set('Access-Control-Allow-Origin','https://'+host);
+    // リクエスト元が開発版のフォームなら開発版のドメインを、本番のフォームなら本番のドメインを設定。
+    // それ以外のリクエスト元はそもそもenvをインスタンス化出来ないのでチェックしない
+    res.set('Access-Control-Allow-Origin','https://' + env.host);
+}
+
+class Environ {
+    constructor(referer) {
+        this.referer = referer;
+        this.host = extractHostDomain(referer);
+    }
+
+    create() {
+        if (this.host === process.env.form_dev) {
+            return new EnvDev(this.referer);
+        }
+        else if (this.host === process.env.form_prod) {
+            return new EnvProd(this.referer);
+        }
+        else {
+            throw new Error('invalid referer.');
+        }
+    }
+}
+
+class EnvDev extends Environ {
+    constructor(referer) {
+        super(referer);
+        this.app_id = process.env.app_id_apply_dev;
+        this.api_token_record = process.env.api_token_apply_record_dev;
+        this.api_token_files = process.env.api_token_apply_files_dev;
+        this.success_redirect_to = 'https://' + this.host + '/apply_complete.html';
+    }
+}
+
+class EnvProd extends Environ {
+    constructor(referer) {
+        super(referer);
+        this.app_id = process.env.app_id_apply_prod;
+        this.api_token_record = process.env.api_token_apply_record_prod;
+        this.api_token_files = process.env.api_token_apply_files_prod;
+        this.success_redirect_to = 'https://' + this.host + '/apply_complete.html';
     }
 }
 
@@ -201,11 +235,4 @@ function extractHostDomain(url) {
     host_domain = host_domain.split(':')[0];
 
     return host_domain;
-}
-
-function isAllowedOrigin(host){
-    if(host == process.env.form_dev)return true;
-    if(host == process.env.form_prod)return true;
-
-    return false;
 }
