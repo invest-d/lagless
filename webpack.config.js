8c10295d1e69dccb6c55651e202c24848e43cd39
atsushianaaki <atushi_anaaki@yahoo.co.jp>
2020-05-30 11:42:03 +0900

    トランスパイル対応：babel-loaderを利用して、生成するjsを古いブラウザでも処理できるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ module.exports = {
         filename: "[name].js",
         path: path.resolve(__dirname, "./public")
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
         new CopyFilePlugin(
             [
