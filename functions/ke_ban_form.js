3d0bb4d3e31df6ce4374118b0e19d47d72ddf5fb
atsushianaaki <atushi_anaaki@yahoo.co.jp>
2020-06-23 01:34:49 +0900

    メール送信出来るようになった

--- functions/ke_ban_form.js
+++ functions/ke_ban_form.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
 
 const Busboy = require("busboy");
-const FormData = require("form-data");
-const axios = require("axios");
+
+const sendgrid = require("@sendgrid/mail");
 
 exports.ke_ban_form = functions.https.onRequest(async (req, res) => {
     if (req.method != "POST") {
@@ -10,121 +10,107 @@ exports.ke_ban_form = functions.https.onRequest(async (req, res) => {
         return;
     }
 
-    console.log(`requested from ${String(req.headers.referer)}`);
+    console.log(`KE-BAN:requested from ${String(req.headers.referer)}`);
 
     // 開発環境か、もしくは本番環境のトークン等の各種データを取得。それ以外のドメインの場合は例外をthrow
     const env = new Environ(req.headers.referer);
 
     setCORS(env, res);
 
-    console.log("フォームデータ受信");
-    const sendObj = {};
-    sendObj.app = env.app_id;
-    const record = {};
+    console.log("KE-BAN:フォームデータ受信");
+    const message = {};
+    message.text = "";
+    message.attachments = [];
+
+    const fieldname_dict = {
+        company: "会社名・屋号名",
+        phone: "電話番号",
+        mail: "メールアドレス",
+        postalCode: "郵便番号",
+        prefecture: "都道府県",
+        address: "住所",
+        streetAddress: "番地・建物名・部屋番号",
+        representative: "代表者名",
+        closingDayFrom: "前払いを希望する稼働期間（開始日）",
+        closingDayTo: "前払いを希望する稼働期間（終了日）",
+        bankCode_Form: "金融機関コード",
+        bankName_Form: "金融機関名",
+        branchCode_Form: "支店コード",
+        branchName_Form: "支店名",
+        deposit_Form: "預金種目",
+        deposit_type: {
+            ordinary: "普通",
+            current: "当座"
+        },
+        accountNumber_Form: "口座番号",
+        accountName_Form: "口座名義",
+        agree: "利用規約に同意する"
+    };
 
     const busboy = new Busboy({ headers: req.headers });
     const allowMimeTypes = ["application/pdf", "image/jpeg", "image/png"];
-    const file_uploads = [];
     busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-        // 請求書データ等のファイルアップロード
         if (String(mimetype) === "application/octet-stream") {
             // 添付ファイルが未入力の場合（application/octet-stream）はスルー
             // 未入力はそもそもフォームでバリデーションをかけているが、2回目以降のフォームの場合は運転免許証画像の欄が未入力のまま送信されてくる。スルーでよい。
             file.resume();
         } else {
-            const upload = new Promise((resolve, reject) => {
-                if (!allowMimeTypes.includes(mimetype.toLocaleLowerCase())) {
-                    console.error("unexpected mimetype.");
-                    console.error(mimetype);
-                    reject({status: 406, message: `添付ファイルは ${allowMimeTypes.map((t) => t.split("/")[1])} のいずれかの形式で送信してください。`});
-                } else {
-                    const form = new FormData();
-                    const ext = String(mimetype).split("/")[1];
-
-                    form.append("file", file, `${fieldname}.${ext}`);
-                    const headers = Object.assign(form.getHeaders(), {
-                        "X-Cybozu-API-Token": env.api_token_files
-                    });
-                    axios.post("https://investdesign.cybozu.com/k/v1/file.json", form, { headers })
-                        .then((result) => {
-                            resolve({
-                                "fieldname": fieldname,
-                                "value": [{"fileKey": result.data.fileKey}]
-                            });
-                        })
-                        .catch((err) => {
-                            console.error("file upload error.");
-                            console.error(err);
-                            reject({status: 500, message: "不明なエラーが発生しました。"});
-                        });
-                }
-            })
-                .catch((err) => {
-                    console.error(`kintoneファイルアップロードエラー：${JSON.stringify(err)}`);
-                    respond_error(res, err);
+            if (!allowMimeTypes.includes(mimetype.toLocaleLowerCase())) {
+                console.error("unexpected mimetype.");
+                console.error(mimetype);
+                respond_error(res, `添付ファイルは ${allowMimeTypes.map((t) => t.split("/")[1])} のいずれかの形式で送信してください。`);
+            } else {
+                // 添付ファイルをbase64エンコードする
+                const file_name = `${fieldname}.${String(mimetype).split("/")[1]}`;
+                file.on("data", (data) => {
+                    const buffer = Buffer.from(data);
+                const file_encoded = buffer.toString("base64");
+                message.attachments.push({
+                    filename: file_name,
+                    content: file_encoded,
+                    type: mimetype.toLocaleLowerCase(),
+                    disposition: "attachment"
                 });
-
-            file_uploads.push(upload);
+                });
+            }
         }
     });
     busboy.on("field", (fieldname, val, fieldnameTruncated, valTruncated) => {
-        record[fieldname]= {"value": val};
+        const value = (() => {
+            // 預金種目の場合に限り "普通" or "当座" に変換する
+            if (val in fieldname_dict["deposit_type"]) {
+                return fieldname_dict["deposit_type"][val];
+            } else {
+                return val;
+            }
+        })();
+        message.text = `${message.text}\n${fieldname_dict[fieldname]}: ${value}`;
     });
-    busboy.on("finish", () => {
-        // ファイルアップロードが全て終わってから、kintoneへのレコード登録を行う。
-        Promise.all(file_uploads)
-            .then((results) => {
-                results.forEach((result) => { record[result["fieldname"]] = {"value": result["value"]}; });
+    busboy.on("finish", async () => {
+        message.from = env.from_address;
+        message.to = env.to_address;
+        message.cc = env.cc_address;
+        message.subject = "軽バンドットコム登録ドライバー様より前払いのお申し込みがありました。";
+        if (message.attachments.length == 0) {
+            delete message.attachments;
+        }
 
-                // 預金種目を日本語に変換。この情報をサーバに送信しない場合（＝既存ユーザの場合）もあるので、そのときは変換もなし
-                if (Object.prototype.hasOwnProperty.call(record, "deposit_Form")) {
-                    const ja_deposit_type = (record["deposit_Form"]["value"] === "ordinary")
-                        ? "普通"
-                        : "当座";
-
-                    record["deposit_Form"] = {"value": ja_deposit_type};
-                }
-
-                // 不要な要素を削除
-                delete record["agree"];
-
-                // sendObjと結合してkintoneにレコード登録可能な形に整える
-                sendObj["record"] = record;
-                console.log("generate sendObj completed.");
-                console.log(JSON.stringify(sendObj));
-
-                // kintoneへの登録開始
-                // 申込みアプリの工務店IDを元に工務店マスタのレコードを参照するため、両方のアプリのAPIトークンが必要
-                const API_TOKEN = `${env.api_token_record},${process.env.api_token_komuten}`;
-
-                const BASE_URL = "https://investdesign.cybozu.com/k/v1/record.json";
-                const BASIC = `Basic ${API_TOKEN}`;
-
-                const headers = {
-                    "Host": `investdesign.cybozu.com:${env.app_id}`,
-                    "X-Cybozu-API-Token": API_TOKEN,
-                    "Authorization": BASIC,
-                    "Content-Type": "application/json",
-                };
-
-                axios.post(BASE_URL, sendObj, { headers })
-                    .then((response) => {
-                        res.status(response.status).json({
-                            "redirect": env.success_redirect_to
-                        });
-                    })
-                    .catch((err) => {
-                        console.error(`kintoneレコード登録エラー：${JSON.stringify(err)}`);
-                        respond_error(res, err);
-                    });
+        sendgrid.setApiKey(process.env.wfi_sendgrid_api_key);
+        await sendgrid.send(message)
+            .catch((error) => {
+                console.error(`KE-BAN:メール送信エラー：${JSON.stringify(error)}`);
+                respond_error(res, error);
             });
+        res.status(200).json({
+            "redirect": env.success_redirect_to
+        });
     });
     busboy.end(req.rawBody);
 });
 
-function respond_error(res, err) {
-    const res_status = ("status" in err) ? err.status : 500;
-    const res_msg = ("message" in err) ? err.message : "サーバーエラーが発生しました。";
+function respond_error(res, error) {
+    const res_status = ("status" in error) ? error.status : 500;
+    const res_msg = ("message" in error) ? error.message : "サーバーエラーが発生しました。";
     res.status(res_status).json({message: res_msg});
 }
 
@@ -139,17 +125,27 @@ class Environ {
     constructor(referer) {
         this.host = extractHostDomain(referer);
 
-        if (this.host === process.env.form_dev) {
+        if (this.host === process.env.form_wfi_dev) {
             // 開発環境
-            this.app_id = process.env.app_id_apply_dev;
-            this.api_token_record = process.env.api_token_apply_record_dev;
-            this.api_token_files = process.env.api_token_apply_files_dev;
+            // 申込内容をメールで送信する
+            this.from_address      = process.env.wfi_dev_from_address;
+            this.to_address        = process.env.wfi_dev_to_address;
+            this.cc_address        = process.env.wfi_dev_cc_address;
+
+            // ゆくゆくはメール送信ではなくkintoneにデータを入れる予定
+            // this.app_id = process.env.app_id_apply_dev;
+            // this.api_token_record = process.env.api_token_apply_record_dev;
+            // this.api_token_files = process.env.api_token_apply_files_dev;
             this.success_redirect_to = `https://${this.host}/apply_complete.html`;
-        } else if (this.host === process.env.form_prod) {
+        } else if (this.host === process.env.form_wfi_prod) {
             // 本番環境
-            this.app_id = process.env.app_id_apply_prod;
-            this.api_token_record = process.env.api_token_apply_record_prod;
-            this.api_token_files = process.env.api_token_apply_files_prod;
+            this.from_address      = process.env.wfi_prod_from_address;
+            this.to_address        = process.env.wfi_prod_to_address;
+            this.cc_address        = process.env.wfi_prod_cc_address;
+
+            // this.app_id = process.env.app_id_apply_prod;
+            // this.api_token_record = process.env.api_token_apply_record_prod;
+            // this.api_token_files = process.env.api_token_apply_files_prod;
             this.success_redirect_to = `https://${this.host}/apply_complete.html`;
         }
         else {
