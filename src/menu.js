9fca01ed1487f359f21b512cb0bb7f44c3c17d12
あつし <atushi_anaaki@yahoo.co.jp>
2020-09-09 16:15:19 +0900

    高田さんからのデザイン指示に従って、新ラグレス案内ページをマークアップ

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,25 @@
 import "../public/app";
 
 $(() => {
-    $("a.skip").on("click", select_skip);
+    $(".skip_button").on("click", select_skip);
 });
 
-function select_skip() {
-    $("#menu").hide();
-    $("#skip").show();
-    return false;
-}
+const hidable_divisions = [
+    "#menu",
+    "#confirm",
+    "#skip_confirmed",
+    "#early",
+    "#late"
+];
+
+const select_skip = () => {
+    show_division("#confirm");
+};
+
+
+const show_division = (target) => {
+    for (const division of hidable_divisions) {
+        $(division).addClass("d-none");
+    }
+    $(target).removeClass("d-none");
+};
