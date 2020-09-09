d7dfc7fed8b1607cc7913262820e40ddcd4b3b58
あつし <atushi_anaaki@yahoo.co.jp>
2020-09-09 16:17:06 +0900

    種々のボタンのデザインと動作を追加

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,10 @@ import "../public/app";
 
 $(() => {
     $(".skip_button").on("click", select_skip);
+    $(".early_button").on("click", select_early);
+    $(".late_button").on("click", select_late);
+    $(".confirm_button").on("click", select_confirm);
+    $(".back_button").on("click", back_to_top);
 });
 
 const hidable_divisions = [
@@ -16,6 +20,21 @@ const select_skip = () => {
     show_division("#confirm");
 };
 
+const select_confirm = () => {
+    show_division("#skip_confirmed");
+};
+
+const select_early = () => {
+    show_division("#early");
+};
+
+const select_late = () => {
+    show_division("#late");
+};
+
+const back_to_top = () => {
+    show_division("#menu");
+};
 
 const show_division = (target) => {
     for (const division of hidable_divisions) {
