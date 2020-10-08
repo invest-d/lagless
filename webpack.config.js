8c4fda0dec362c8ab05a1cd709d558eb4740b58b
鈴木徳 <suzuki@invest-d.com>
2020-10-08 10:53:29 +0900

    async関数をbabelでbundle可能にするため、plugin-transform-runtimeをインストール： https://note.kiriukun.com/entry/20200704-regeneratorRuntime-is-not-defined

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = (env, argv) => {
                     use: {
                         loader: "babel-loader",
                         options: {
-                            presets: ["@babel/preset-env"]
+                            presets: ["@babel/preset-env"],
+                            plugins: ["@babel/plugin-transform-runtime"],
                         }
                     }
                 }
