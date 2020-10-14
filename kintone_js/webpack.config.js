44e86d481cfee6abb1804e8dc525a1e0a30968ae
鈴木徳 <suzuki@invest-d.com>
2020-10-14 15:10:29 +0900

    kintone_jsのwebpack buildにdevelopment modeを追加

--- kintone_js/webpack.config.js
+++ kintone_js/webpack.config.js
@@ -1,27 +1,62 @@
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.png$/i,
-                use: ["url-loader"]
+module.exports = (env, argv) => {
+    const IS_DEVELOPMENT = argv.mode === "development";
+    if (IS_DEVELOPMENT) {
+        return {
+            mode: "development",
+            devtool:"source-map",
+            module: {
+                rules: [
+                    {
+                        test: /\.png$/i,
+                        use: ["url-loader"]
+                    }
+                ]
+            },
+
+            entry: {
+                output_csv: "./output_csv.js",
+                generate_invoice_button: "./generate_invoice_button.js",
+                add_to_queue_button: "./add_to_queue_button.js",
+                unsend_invoice_button: "./unsend_invoice_button.js",
+                color_exceeding_payment: "./color_exceeding_payment.js",
+                generate_acceptance_letter_button: "./generate_acceptance_letter_button.js"
+            },
+
+            // ファイルの出力設定
+            output: {
+                //  出力ファイルのディレクトリ名
+                path: `${__dirname}/dist`,
+                // 出力ファイル名
+                filename: "[name].js"
             }
-        ]
-    },
+        };
+    } else {
+        return {
+            module: {
+                rules: [
+                    {
+                        test: /\.png$/i,
+                        use: ["url-loader"]
+                    }
+                ]
+            },
 
-    entry: {
-        output_csv: "./output_csv.js",
-        generate_invoice_button: "./generate_invoice_button.js",
-        add_to_queue_button: "./add_to_queue_button.js",
-        unsend_invoice_button: "./unsend_invoice_button.js",
-        color_exceeding_payment: "./color_exceeding_payment.js",
-        generate_acceptance_letter_button: "./generate_acceptance_letter_button.js"
-    },
+            entry: {
+                output_csv: "./output_csv.js",
+                generate_invoice_button: "./generate_invoice_button.js",
+                add_to_queue_button: "./add_to_queue_button.js",
+                unsend_invoice_button: "./unsend_invoice_button.js",
+                color_exceeding_payment: "./color_exceeding_payment.js",
+                generate_acceptance_letter_button: "./generate_acceptance_letter_button.js"
+            },
 
-    // ファイルの出力設定
-    output: {
-    //  出力ファイルのディレクトリ名
-        path: `${__dirname}/dist`,
-        // 出力ファイル名
-        filename: "[name].js"
+            // ファイルの出力設定
+            output: {
+                //  出力ファイルのディレクトリ名
+                path: `${__dirname}/dist`,
+                // 出力ファイル名
+                filename: "[name].js"
+            }
+        };
     }
 };
