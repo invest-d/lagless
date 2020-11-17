e2d0e9a071039eeb2f1e351c14574a40a2aca5e8
Issei Yagihara <yagio0213@gmail.com>
2020-11-17 18:15:29 +0900

    LAGLESS-142:　申し込み後に、自動でメールまたはSMSを送信するスクリプトを作成

--- functions/send_apply.js
+++ functions/send_apply.js
@@ -3,15 +3,110 @@ const functions = require("firebase-functions");
 const Busboy = require("busboy");
 const FormData = require("form-data");
 const axios = require("axios");
+const sendMail = require("./sendmail_frame.js");
+const fs = require("fs");
+const path = require("path");
+const auto_text = fs.readFileSync(path.join(__dirname, "autoMail_template.txt"), "utf8");
+const SMS_text = fs.readFileSync(path.join(__dirname, "autoSMS_template.txt"), "utf8");
+const date = new Date ();
+
+// sms送信用
+const AWS = require("aws-sdk");
+AWS.config.update({
+    accessKeyId: process.env.SNS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.SNS_SECRET_KEY,
+    region: "ap-northeast-1"
+});
+const sns = new AWS.SNS();
 
 exports.send_apply = functions.https.onRequest((req, res) => {
     postToKintone(req, res)
-        .then((post_succeed) => {
+        .then(async (post_succeed) => {
+            const payload = {
+                "app": 96,
+                "fields": ["支払元口座", "service"],
+                "query": `id = "${post_succeed.record.constructionShopId.value}"`,
+            };
+            const token = process.env.api_token_komuten_sendauto;
+            const headers = {
+                "Host": "investdesign.cybozu.com:96",
+                "X-Cybozu-API-Token": token,
+                "Authorization": `Basic ${token}`,
+                "Content-Type": "application/json",
+            };
+        
+            // 工務店マスタから情報（支払元口座、商品名）を抽出
+            const result = await axios({
+                method: "get",
+                url: "https://investdesign.cybozu.com/k/v1/records.json",
+                data: payload,
+                headers: headers
+            });
+
+            // 申し込み時の日時を取得
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const dateNow = `${year}年 ${month}月 ${day}日`;
+            
+            // 支払元口座、商品名を取得
+            const productName = result.data.records[0]["service"]["value"];
+            const accountFrom = result.data.records[0]["支払元口座"]["value"];
+
+            // 支払い元口座によって変化する会社名、また商品名と申し込み時の日付をrecordに追加
+            if (accountFrom === "ID") {
+                post_succeed.record["contractor"] = {"value": "ラグレス２合同会社"};
+            } else if (accountFrom === "LAGLESS") {
+                post_succeed.record["contractor"] = {"value": "ラグレス合同会社"};
+            }
+            post_succeed.record["product_name"] = {"value": productName};
+            post_succeed.record["date_now"] = {"value": dateNow};
+
+            // 既存の顧客の場合、預金項目のkeyがrecordに存在しないため空欄で追加する
+            if (!checkUser(post_succeed.record)) {
+                post_succeed.record["deposit_Form"] = {"value": ""};
+            }
+
+            const number = post_succeed.record["phone"]["value"].replace(0, "+81");
+            let subject = "dandoliPay";
+            // 登録情報のメールアドレスが空欄の時のみ、SMSを送信
+            if (!post_succeed.record["mail"]["value"]) {
+                if (productName=="リノベ不動産Payment") subject = "renovePay";
+                if (productName=="ラグレス") subject = "lagless";
+                sendSMS(number, subject, replaceMail(SMS_text, post_succeed.record), (err, result) => {
+                    console.log("RESULTS: ",err,result);
+                });
+            }
+            else 
+            {
+                const options = {
+                    host: "smtp.sendgrid.net", 
+                    port: 587, 
+                    requiresAuth: true,
+                    auth: {
+                        user: process.env.SENDGRID_USERNAME,
+                        pass: process.env.SENDGRID_PASSWORD,
+                    },
+                };
+
+                const mail = {
+                    from: "lagless@invest-d.com",
+                    to: `${post_succeed.record["mail"]["value"]}`,
+                    bcc: "lagless@invest-d.com",
+                    subject: `【 ${productName} 】お申込みいただきありがとうございます。`,
+                    text: replaceMail(auto_text, post_succeed.record),
+                };
+
+                // 自動応答メール送信
+                sendMail(mail, options);
+            }
+
             res.status(post_succeed.status).json({
                 "redirect": post_succeed.redirect_to
             });
         })
         .catch((err) => {
+            console.error(err);
             respond_error(res, err);
         });
 });
@@ -162,7 +257,8 @@ function postToKintone(req, res) {
 
                 resolve({
                     status: kintone_post_response.status,
-                    redirect_to: env.success_redirect_to
+                    redirect_to: env.success_redirect_to,
+                    record: record
                 });
             } else {
                 // 念のためのreject
@@ -221,13 +317,13 @@ function respond_error(res, err) {
 // 開発or本番以外のドメインからのリクエストはそもそもenvをインスタンス化出来ないのでチェックしない
 function setCORS(env, res){
     // リクエスト元が開発版のフォームなら開発版のドメインを、本番のフォームなら本番のドメインを設定。
-    res.set("Access-Control-Allow-Origin",`${env.host}`);
+    res.set("Access-Control-Allow-Origin","http://localhost:5000");
 }
 
 class Environ {
     constructor(host) {
         this.host = host;
-        if (this.host === process.env.form_dev) {
+        if (this.host === "http://localhost:5000") {
             // 開発環境
             this.app_id = process.env.app_id_apply_dev;
             this.api_token_record = process.env.api_token_apply_record_dev;
@@ -244,4 +340,34 @@ class Environ {
             throw new Error(`invalid host: ${this.host}`);
         }
     }
+}
+
+// 別ファイルとして作成したメールのテンプレートにrecord内の変数を入れ込む
+function replaceMail(template, record) {
+    const pattern = /{\s*(\w+?)\s*}/g;
+    return template.replace(pattern, (_, token) => {
+        return record[token]["value"] || "";
+    });
+}
+
+// 本来はクエリパラメータで判断すべきだが、ブラウザによって挙動が違うため、
+// 預金種目の有無にて既存か新規の顧客か判断 (既存の顧客の場合、record内の預金項目のkeyがなくなる)
+function checkUser(record) {
+    return Object.prototype.hasOwnProperty.call(record, "deposit_Form");
+}
+
+// SMS送信用関数
+function sendSMS(to_number, subject, message, cb) {
+
+    sns.publish({
+        PhoneNumber: to_number,
+        Message: message,
+        MessageAttributes: {
+            "AWS.SNS.SMS.SenderID" : {
+                DataType : "String",
+                StringValue: subject
+            }
+        }
+    }, cb);
+  
 }
