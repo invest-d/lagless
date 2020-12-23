bfb94df502b522ed84e88ebd3a37016e29be0e90
鈴木徳 <suzuki@invest-d.com>
2020-12-23 10:32:15 +0900

    申込可能な稼働期間が複数ある場合、案内ページにも複数の期間を表示するように修正。申込可能な稼働期間がない場合、申込フォームから申込を送信できないように修正。

--- src/ke_ban.js
+++ src/ke_ban.js
@@ -37,48 +37,40 @@ export const getTodayDate = () => {
     }
 };
 
-export const get_pay_term_start_date = (target_date) => {
+export const get_pay_term_start_date = (base_date) => {
     // target_dateが属する申込可能な稼働期間の開始日を取得する
-    let start_date = target_date;
+    let start_date = base_date;
     while (start_date.date() % 5 !== 1) {
         start_date = start_date.subtract(1, "day");
     }
     return start_date;
 };
-export const get_pay_term_end_date = (target_date) => {
-    // target_dateが属する申込可能な稼働期間の終了日を取得する
-    if (get_pay_term_start_date(target_date).date() === 26) {
+export const get_pay_term_end_date = (base_date) => {
+    // base_dateが属する申込可能な稼働期間の終了日を取得する
+    if (get_pay_term_start_date(base_date).date() === 26) {
         // 最終タームの場合、前払い対象の終了日は月末
-        return target_date.endOf("month");
+        return base_date.endOf("month");
     } else {
-        let end_date = target_date;
+        let end_date = base_date;
         while (end_date.date() % 5 !== 0) {
             end_date = end_date.add(1, "day");
         }
         return end_date;
     }
 };
-export const get_next_business_date = (target_date) => {
-    let next_date = target_date.add(1, "day");
+export const get_next_business_date = (base_date) => {
+    let next_date = base_date.add(1, "day");
     while([0, 6].includes(next_date.day())) {
         next_date = next_date.add(1, "day");
     }
     return next_date;
 };
 
-// フォームを開いた時点において、前払対象になる期間を確定する
-$(() => {
+export const get_terms_before_and_after = (today) => {
     // 前払い可能な期間は原則的に5日ごとの区切り。
     // 1日～5日、6日～10日、……、21日〜25日。
     // 前払い可能な期間の開始日の翌日から終了日の翌営業日までが申込期間。
-    // ページを開く度に前払い期間と申込期間のテーブルを用意する。
-    // 現在の日付が当てはまっている申込期間の数だけ前払い期間のドロップダウンを生成する。
-
-    // 26日〜月末日の期間については申込対象外。
-    // 申込対象外の期間だけがドロップダウンに表示される場合に限り、「現在お申込み頂ける稼働期間はございません。」と表示。
-
     // 現在の日付が属する五十日について、開始日と終了日を求める
-    const today = getTodayDate();
     const terms = {
         prev: {
             pay: {
@@ -133,6 +125,19 @@ $(() => {
     terms.now.apply.end = get_next_business_date(terms.now.pay.end);
     terms.next.apply.end = get_next_business_date(terms.next.pay.end);
 
+    return terms;
+};
+
+// フォームを開いた時点において、前払対象になる期間を確定する
+$(() => {
+    // ページを開く度に前払い期間と申込期間のテーブルを用意する。
+    // 現在の日付が当てはまっている申込期間の数だけ前払い期間のドロップダウンを生成する。
+
+    // 26日〜月末日の期間については申込対象外。
+    // 申込対象外の期間だけがドロップダウンに表示される場合に限り、「現在お申込み頂ける稼働期間はございません。」と表示。
+    const today = getTodayDate();
+    const terms = get_terms_before_and_after(today);
+
     // 現在の日付がどの申込期間に入っているかを判定して、選択肢に加えていく
     const choice_phrases = [];
     if (today.isBetween(terms.prev.apply.start, terms.prev.apply.end, null, "[]")) {
@@ -165,6 +170,10 @@ $(() => {
         unavailable_option.attr("value", unavailable_message);
         unavailable_option.text(unavailable_message);
         term_select.append(unavailable_option);
+
+        // 画面全体を非表示にもする
+        $("#form_id").addClass("d-none");
+        $("#unavailable").removeClass("d-none");
     }
 });
 
