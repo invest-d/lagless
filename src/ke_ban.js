a5872e0240165428c4efde021e8df62d137c87a7
鈴木徳 <suzuki@invest-d.com>
2020-12-24 14:22:12 +0900

    todayを一つに固定するよう修正。31日始まりという本来存在ない五十日が算出されてしまう不具合を修正。2020年内は申込可能な稼働期間が存在しない判定をするように機能追加

--- src/ke_ban.js
+++ src/ke_ban.js
@@ -37,14 +37,19 @@ export const getTodayDate = () => {
     }
 };
 
+export const TODAY = getTodayDate();
+
 export const get_pay_term_start_date = (base_date) => {
-    // target_dateが属する申込可能な稼働期間の開始日を取得する
+    // base_dateが属する申込可能な稼働期間の開始日を取得する
     let start_date = base_date;
-    while (start_date.date() % 5 !== 1) {
+    while (start_date.date() % 5 !== 1 || start_date.date() === 31) {
+        // start_date.date()が五十日のスタートとなる日付（つまり1, 6, 11, ... の日付）になるまで計算する。
+        // 31日スタートの五十日は存在しないため、別途除外
         start_date = start_date.subtract(1, "day");
     }
     return start_date;
 };
+
 export const get_pay_term_end_date = (base_date) => {
     // base_dateが属する申込可能な稼働期間の終了日を取得する
     if (get_pay_term_start_date(base_date).date() === 26) {
@@ -58,7 +63,11 @@ export const get_pay_term_end_date = (base_date) => {
         return end_date;
     }
 };
+
 export const get_next_business_date = (base_date) => {
+    // base_date以外の、base_dateの翌日以降の営業日を取得する。
+    // 現状、曜日での判定のみ利用可能
+    // FIXME: その他日本の祝日やカスタム休日にも対応する
     let next_date = base_date.add(1, "day");
     while([0, 6].includes(next_date.day())) {
         next_date = next_date.add(1, "day");
@@ -66,11 +75,10 @@ export const get_next_business_date = (base_date) => {
     return next_date;
 };
 
-export const get_terms_before_and_after = (today) => {
-    // 前払い可能な期間は原則的に5日ごとの区切り。
-    // 1日～5日、6日～10日、……、21日〜25日。
-    // 前払い可能な期間の開始日の翌日から終了日の翌営業日までが申込期間。
-    // 現在の日付が属する五十日について、開始日と終了日を求める
+export const get_terms_prev_now_next = (today) => {
+    // 稼働期間：五十日の区切り
+    // 申込期間：稼働期間の開始日〜稼働期間の終了日の翌営業日
+    // todayを現在の五十日に属する日付として、前後の稼働期間と申込期間を求める。
     const terms = {
         prev: {
             pay: {
@@ -104,28 +112,52 @@ export const get_terms_before_and_after = (today) => {
         }
     };
 
-    terms.now.pay.start = get_pay_term_start_date(today);
-    terms.now.pay.end = get_pay_term_end_date(today);
+    terms.now.pay.start     = get_pay_term_start_date(today);
+    terms.now.pay.end       = get_pay_term_end_date(today);
 
     // 現在の五十日の直前の五十日について、開始日と終了日を求める
-    terms.prev.pay.start = get_pay_term_start_date(terms.now.pay.start.subtract(1, "day"));
-    terms.prev.pay.end = get_pay_term_end_date(terms.now.pay.start.subtract(1, "day"));
+    terms.prev.pay.start    = get_pay_term_start_date(terms.now.pay.start.subtract(1, "day"));
+    terms.prev.pay.end      = get_pay_term_end_date(terms.now.pay.start.subtract(1, "day"));
 
     // 現在の五十日の直後の五十日について、開始日と終了日を求める
-    terms.next.pay.start = get_pay_term_start_date(terms.now.pay.end.add(1, "day"));
-    terms.next.pay.end = get_pay_term_end_date(terms.now.pay.end.add(1, "day"));
+    terms.next.pay.start    = get_pay_term_start_date(terms.now.pay.end.add(1, "day"));
+    terms.next.pay.end      = get_pay_term_end_date(terms.now.pay.end.add(1, "day"));
 
     // それぞれの五十日について、申込可能期間を求める
     // 申込可能期間の開始日は常に五十日の開始日に等しい
-    terms.prev.apply.start = terms.prev.pay.start;
-    terms.now.apply.start = terms.now.pay.start;
-    terms.next.apply.start = terms.next.pay.start;
+    terms.prev.apply.start  = terms.prev.pay.start;
+    terms.now.apply.start   = terms.now.pay.start;
+    terms.next.apply.start  = terms.next.pay.start;
     // 申込可能期間の終了日は五十日の終了日の翌営業日
-    terms.prev.apply.end = get_next_business_date(terms.prev.pay.end);
-    terms.now.apply.end = get_next_business_date(terms.now.pay.end);
-    terms.next.apply.end = get_next_business_date(terms.next.pay.end);
+    terms.prev.apply.end    = get_next_business_date(terms.prev.pay.end);
+    terms.now.apply.end     = get_next_business_date(terms.now.pay.end);
+    terms.next.apply.end    = get_next_business_date(terms.next.pay.end);
 
     return terms;
+};
+
+export const get_available_terms = (terms, today) => {
+    // 単純な五十日の期間のうち、システム上受付可能な五十日のみをArrayにして返す
+    const available_terms = [];
+    // Object.keys(terms)を使いたいが、順番が保証されない(下記参照)ので手打ちでkeyの配列を定義する
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/keys#Using_Object.keys
+    for (const term of ["prev", "now", "next"]) {
+        if (today.isBetween(terms[term].apply.start, terms[term].apply.end, null, "[]")) {
+            // 26日以降スタートの稼働期間は除外する
+            if (terms[term].pay.start.date() >= 26) {
+                continue;
+            }
+
+            // 2020年内スタートの稼働期間は除外する
+            if (terms[term].pay.start.year() < 2021) {
+                continue;
+            }
+
+            available_terms.push(terms[term]);
+        }
+    }
+
+    return available_terms;
 };
 
 // フォームを開いた時点において、前払対象になる期間を確定する
@@ -133,37 +165,21 @@ $(() => {
     // ページを開く度に前払い期間と申込期間のテーブルを用意する。
     // 現在の日付が当てはまっている申込期間の数だけ前払い期間のドロップダウンを生成する。
 
-    // 26日〜月末日の期間については申込対象外。
-    // 申込対象外の期間だけがドロップダウンに表示される場合に限り、「現在お申込み頂ける稼働期間はございません。」と表示。
-    const today = getTodayDate();
-    const terms = get_terms_before_and_after(today);
-
-    // 現在の日付がどの申込期間に入っているかを判定して、選択肢に加えていく
-    const choice_phrases = [];
-    if (today.isBetween(terms.prev.apply.start, terms.prev.apply.end, null, "[]")) {
-        choice_phrases.push(`${terms.prev.pay.start.format("YYYY年MM月DD日")}から${terms.prev.pay.end.format("YYYY年MM月DD日")}まで`);
+    const terms = get_terms_prev_now_next(TODAY);
+    const display_terms = [];
+    for (const term of get_available_terms(terms, TODAY)) {
+        display_terms.push(`${term.pay.start.format("YYYY年MM月DD日")}から${term.pay.end.format("YYYY年MM月DD日")}まで`);
     }
-
-    if (today.isBetween(terms.now.apply.start, terms.now.apply.end, null, "[]")) {
-        choice_phrases.push(`${terms.now.pay.start.format("YYYY年MM月DD日")}から${terms.now.pay.end.format("YYYY年MM月DD日")}まで`);
-    }
-
-    if (today.isBetween(terms.next.apply.start, terms.next.apply.end, null, "[]")) {
-        choice_phrases.push(`${terms.next.pay.start.format("YYYY年MM月DD日")}から${terms.next.pay.end.format("YYYY年MM月DD日")}まで`);
-    }
-
-    // 不可能な期間を除外
-    const selectable = choice_phrases.filter((p) => !p.includes("26日から"));
 
     const term_select = $("#targetTerm");
-    selectable.forEach((item) => {
+    display_terms.forEach((item) => {
         const option = $("<option>");
         option.attr("value", item);
         option.text(item);
         term_select.append(option);
     });
 
-    // 選択可能な期間がない場合はその旨の選択肢を表示する
+    // 申込対象外の期間だけがドロップダウンに表示される場合に限り、「現在お申込み頂ける稼働期間はございません。」と表示。
     if (term_select.children().length === 0) {
         const unavailable_option = $("<option>");
         const unavailable_message = "現在お申込み頂ける稼働期間はございません。";
