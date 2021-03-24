e62e008532b522bbd7b1607cfec815c7ef16928c
鈴木徳 <suzuki@invest-d.com>
2021-03-24 10:31:07 +0900

    処理をスタートするボタンと、キャンセルするボタンを交互に表示するだけのスクリプトを作成

---
+++ kintone_js/96/button_convert_workship_list.js
@@ -0,0 +1,48 @@
+/*
+    Version 1
+    Workshipで保存したhtmlファイルを解析して、
+    kintone工務店マスタに取り込み可能なcsvファイルを生成する。
+*/
+
+const execute_button_id = "convertWorkshipList";
+const execute_button_title = "Workship企業リストを変換";
+const cancel_button_id = "cancelConvertWorkshipList";
+const cancel_button_title = "キャンセル";
+
+const workship_theme_color = "#59afa9";
+
+export const needShow = () => {
+    // 増殖バグ防止
+    return document.getElementById(execute_button_id) === null
+        && document.getElementById(cancel_button_id) === null;
+};
+
+export const createExecuteButton = () => {
+    return createButton(execute_button_id, execute_button_title, clickExecuteButton);
+};
+
+export const createCancelButton = () => {
+    const cancel_button = createButton(cancel_button_id, cancel_button_title, clickCancelButton);
+    cancel_button.style.display = "none";
+    cancel_button.style.backgroundColor = workship_theme_color;
+    return cancel_button;
+};
+
+const createButton = (button_name, button_title, click_event) => {
+    const button = document.createElement("button");
+    button.id = button_name;
+    button.innerText = button_title;
+    button.addEventListener("click", click_event);
+    return button;
+};
+
+const clickExecuteButton = () => {
+    // キャンセルボタンを表示する
+    document.getElementById(cancel_button_id).style.display = "inline";
+    document.getElementById(execute_button_id).style.display = "none";
+};
+
+const clickCancelButton = () => {
+    document.getElementById(execute_button_id).style.display = "inline";
+    document.getElementById(cancel_button_id).style.display = "none";
+};
