7bffc2ed0e54882bedb328d2e0b4f6d90087360c
鈴木徳 <suzuki@invest-d.com>
2021-04-19 17:21:47 +0900

    [リファクタ]ファイルリネーム

--- kintone_js/app_collect.js
+++ kintone_js/app_collect.js
@@ -5,7 +5,7 @@
 
 import * as abort from "./app_collect/logics_abort_manual_adding_to_queue";
 import * as queue from "./app_collect/logics_add_to_queue_button";
-import * as copy from "./app_collect/copy_credit_button";
+import * as copy from "./app_collect/logics_copy_credit_button";
 
 (function () {
     "use strict";
