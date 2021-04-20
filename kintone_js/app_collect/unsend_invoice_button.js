695b31296ea0f1b1defc005f28b332052fe4ec2b
鈴木徳 <suzuki@invest-d.com>
2021-04-20 11:43:03 +0900

    [リファクタ]定義を外出し

--- kintone_js/app_collect/unsend_invoice_button.js
+++ kintone_js/app_collect/unsend_invoice_button.js
@@ -12,18 +12,56 @@
 import { getParentAndChildCollectRecords, updateStatus } from "./logics_add_to_queue_button";
 import { schema_collect } from "../162/schema";
 
+const APP_ID_COLLECT                    = kintone.app.getId();
+const fieldStatus_COLLECT               = schema_collect.fields.properties.collectStatus.code;
+const statusUnsend_COLLECT              = schema_collect.fields.properties.collectStatus.options.クラウドサイン承認済み.label;
+const unsendTarget = [
+    schema_collect.fields.properties.collectStatus.options.振込依頼書送信可.label,
+    schema_collect.fields.properties.collectStatus.options.振込依頼書送信済み.label
+];
+const fieldParentCollectRecord_COLLECT  = schema_collect.fields.properties.parentCollectRecord.code;
+const isParent_COLLECT                  = schema_collect.fields.properties.parentCollectRecord.options.true.label;
+
+function needShowButton(record) {
+    const is_not_displayed = document.getElementById("unsendInvoice") === null;
+
+    // 振込依頼書送信済みの（もしくはもうすぐ送信される）親レコードの場合のみ表示
+    const is_parent = (unsendTarget.includes(record[fieldStatus_COLLECT]["value"])) && ((record[fieldParentCollectRecord_COLLECT]["value"]).includes(isParent_COLLECT));
+    return is_not_displayed && is_parent;
+}
+
+function createUnsendInvoiceButton(record) {
+    const unsendInvoice = document.createElement("button");
+    unsendInvoice.id = "unsendInvoice";
+    unsendInvoice.innerText = "このレコードを振込依頼書送信前の状態に戻す";
+    unsendInvoice.addEventListener("click", clickUnsendInvoice.bind(null, record));
+    return unsendInvoice;
+}
+
+async function clickUnsendInvoice (record) {
+    const clicked_ok = confirm("このレコードの振込依頼書を未送信の状態（送信前の確認中の状態）に戻してもよろしいですか？");
+    if (!clicked_ok) {
+        alert("処理は中断されました。");
+        return;
+    }
+
+    const collects_resp = await getParentAndChildCollectRecords(record)
+        .catch((err) => {
+            throw new Error(`この親レコードに関連する子レコードの取得中にエラーが発生しました。\n\nエラーログ：${err}`);
+        });
+
+    await updateStatus(collects_resp.records, statusUnsend_COLLECT)
+        .catch((err) => {
+            throw new Error(`親子レコードの状態フィールドの更新中にエラーが発生しました。\n\nエラーログ：${err}`);
+        });
+
+    alert(`親子レコードの状態を「${statusUnsend_COLLECT}」に戻しました。\n「振込依頼書を作成」ボタンから振込依頼書をもう一度作り直すことができます。`);
+    alert("レコード一覧画面に戻ります。");
+    window.location.href = `/k/${APP_ID_COLLECT}/`;
+}
+
 (function() {
     "use strict";
-
-    const APP_ID_COLLECT                    = kintone.app.getId();
-    const fieldStatus_COLLECT               = schema_collect.fields.properties.collectStatus.code;
-    const statusUnsend_COLLECT              = schema_collect.fields.properties.collectStatus.options.クラウドサイン承認済み.label;
-    const unsendTarget = [
-        schema_collect.fields.properties.collectStatus.options.振込依頼書送信可.label,
-        schema_collect.fields.properties.collectStatus.options.振込依頼書送信済み.label
-    ];
-    const fieldParentCollectRecord_COLLECT  = schema_collect.fields.properties.parentCollectRecord.code;
-    const isParent_COLLECT                  = schema_collect.fields.properties.parentCollectRecord.options.true.label;
 
     kintone.events.on("app.record.detail.show", (event) => {
         if (needShowButton(event.record)) {
@@ -31,42 +69,4 @@ import { schema_collect } from "../162/schema";
             kintone.app.record.getHeaderMenuSpaceElement().appendChild(button);
         }
     });
-
-    function needShowButton(record) {
-        const is_not_displayed = document.getElementById("unsendInvoice") === null;
-
-        // 振込依頼書送信済みの（もしくはもうすぐ送信される）親レコードの場合のみ表示
-        const is_parent = (unsendTarget.includes(record[fieldStatus_COLLECT]["value"])) && ((record[fieldParentCollectRecord_COLLECT]["value"]).includes(isParent_COLLECT));
-        return is_not_displayed && is_parent;
-    }
-
-    function createUnsendInvoiceButton(record) {
-        const unsendInvoice = document.createElement("button");
-        unsendInvoice.id = "unsendInvoice";
-        unsendInvoice.innerText = "このレコードを振込依頼書送信前の状態に戻す";
-        unsendInvoice.addEventListener("click", clickUnsendInvoice.bind(null, record));
-        return unsendInvoice;
-    }
-
-    async function clickUnsendInvoice (record) {
-        const clicked_ok = confirm("このレコードの振込依頼書を未送信の状態（送信前の確認中の状態）に戻してもよろしいですか？");
-        if (!clicked_ok) {
-            alert("処理は中断されました。");
-            return;
-        }
-
-        const collects_resp = await getParentAndChildCollectRecords(record)
-            .catch((err) => {
-                throw new Error(`この親レコードに関連する子レコードの取得中にエラーが発生しました。\n\nエラーログ：${err}`);
-            });
-
-        await updateStatus(collects_resp.records, statusUnsend_COLLECT)
-            .catch((err) => {
-                throw new Error(`親子レコードの状態フィールドの更新中にエラーが発生しました。\n\nエラーログ：${err}`);
-            });
-
-        alert(`親子レコードの状態を「${statusUnsend_COLLECT}」に戻しました。\n「振込依頼書を作成」ボタンから振込依頼書をもう一度作り直すことができます。`);
-        alert("レコード一覧画面に戻ります。");
-        window.location.href = `/k/${APP_ID_COLLECT}/`;
-    }
 })();
