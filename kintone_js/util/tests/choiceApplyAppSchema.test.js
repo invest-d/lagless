08abe11057246cb0fd6431b3edd5b937b29feb0a
鈴木徳 <suzuki@invest-d.com>
2021-09-24 09:28:10 +0900

    申込アプリのスキーマを選択するロジックをutilに定義した

---
+++ kintone_js/util/tests/choiceApplyAppSchema.test.js
@@ -0,0 +1,19 @@
+import { detectApp, UnknownAppError } from "../choiceApplyAppSchema";
+
+test("159を渡すと開発判定になる", () => {
+    expect(detectApp(159)).toBe("dev");
+});
+
+test("161を渡すと本番判定になる", () => {
+    expect(detectApp(161)).toBe("prod");
+});
+
+test("不明なidを渡すと例外をthrowする", () => {
+    const detectError = () => detectApp(160);
+    expect(detectError).toThrowError(UnknownAppError);
+});
+
+test("nullを渡すと例外をthrowする", () => {
+    const detectError = () => detectApp(null);
+    expect(detectError).toThrowError(UnknownAppError);
+});
