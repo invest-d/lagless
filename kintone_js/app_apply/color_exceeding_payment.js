60a050d0e6006d6ec4caf9840c6442f704049e3f
鈴木徳 <suzuki@invest-d.com>
2021-09-27 12:23:10 +0900

    リファクタ: スキーマ導入

--- kintone_js/app_apply/color_exceeding_payment.js
+++ kintone_js/app_apply/color_exceeding_payment.js
@@ -7,7 +7,30 @@ import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 extend(isSameOrAfter);
 locale("ja");
 
-(function() {
+import { getApplyAppSchema, UnknownAppError } from "../util/choiceApplyAppSchema";
+const schema = (() => {
+    try {
+        return getApplyAppSchema(kintone.app.getId());
+    } catch (e) {
+        if (e instanceof UnknownAppError) {
+            alert("不明なアプリです。申込アプリで実行してください。");
+        } else {
+            console.error(e);
+            const additional_info = e.message ?? JSON.stringify(e);
+            alert("途中で処理に失敗しました。システム管理者に連絡してください。"
+                + "\n追加の情報: "
+                + `\n${additional_info}`);
+        }
+    }
+})();
+if (!schema) throw new Error();
+const applyFields = schema.fields.properties;
+const paymentDateApply = applyFields.paymentDate.code;
+const statusApply = applyFields.状態.code;
+const paidStatusApply = applyFields.状態.options.実行完了.label;
+const cancelledStatusApply = applyFields.状態.options.取下げ.label;
+
+(function () {
     "use strict";
     kintone.events.on("app.record.index.show", (event) => {
         // https://developer.cybozu.io/hc/ja/articles/201941964#step1
@@ -19,20 +42,20 @@ locale("ja");
 
         // https://developer.cybozu.io/hc/ja/articles/201942004#step3
         // viewに目的のフィールドを含んでいない場合、getFieldElementsはnullを返す
-        const date_elements = kintone.app.getFieldElements("paymentDate");
+        const date_elements = kintone.app.getFieldElements(paymentDateApply);
         if (!date_elements) return;
 
         const today = dayjs();
 
         date_elements.forEach((element, index) => {
             // 実行済もしくは実行不要のレコードには色をつけない
-            if (["実行完了", "取下げ"].includes(records[index]["状態"]["value"])) {
+            if ([paidStatusApply, cancelledStatusApply].includes(records[index][statusApply]["value"])) {
                 return;
             }
 
             // 今日の日付と支払日列の値を比較し、今日 >= 支払日の場合に色を付ける
-            if (records[index]["paymentDate"]["value"] && // 列に値が入力されている場合のみ
-                today.isSameOrAfter(dayjs(records[index]["paymentDate"]["value"]))) {
+            if (records[index][paymentDateApply]["value"] && // 列に値が入力されている場合のみ
+                today.isSameOrAfter(dayjs(records[index][paymentDateApply]["value"]))) {
                 element.style.backgroundColor = exceeding_color;
             }
         });
