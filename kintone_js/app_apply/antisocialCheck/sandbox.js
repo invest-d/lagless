40810b840f7ea54f9209967874f5493396da67b3
鈴木徳 <suzuki@invest-d.com>
2021-10-07 17:39:43 +0900

    細かいテスト用のファイル名をわかりやすく変更

---
+++ kintone_js/app_apply/antisocialCheck/sandbox.js
@@ -0,0 +1,36 @@
+import { CLIENT } from "../../util/kintoneAPI";
+import { schema_28 } from "../../28/schema";
+import { schema_79 } from "../../79/schema";
+import { addToukiRecord, buildToukiRecord } from "./fetchTouki";
+
+(function () {
+    // eslint-disable-next-line no-unused-vars
+    kintone.events.on("app.record.detail.show", (event) => {
+        kintone.app.record.getHeaderMenuSpaceElement().appendChild(createButton());
+    });
+})();
+
+const buttonId = "sandbox";
+const buttonTitle = "サンドボックス";
+const createButton = () => {
+    const button = document.createElement("button");
+    button.id = buttonId;
+    button.innerText = buttonTitle;
+    button.addEventListener("click", clickButton);
+    return button;
+};
+
+const clickButton = async () => {
+    const company = await CLIENT.record.getRecord({
+        app: schema_28.id.appId,
+        id: "6189"
+    });
+    const exam = await CLIENT.record.getRecord({
+        app: schema_79.id.appId,
+        id: "4065"
+    });
+    const toukiRecord = buildToukiRecord(company.record, exam.record);
+    console.log(toukiRecord);
+    const newId = await addToukiRecord(toukiRecord);
+    console.log(newId);
+};
