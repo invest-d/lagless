9588e02968b8af731eb3ab5db1e1a8e316a4797e
鈴木徳 <suzuki@invest-d.com>
2021-10-07 09:55:54 +0900

    fix typo

--- kintone_js/app_apply/antisocialCheck/testableLogics.js
+++ kintone_js/app_apply/antisocialCheck/testableLogics.js
@@ -61,7 +61,7 @@ export const getSearchQuery = ({
  * @property {string} 訂正区分
  * @property {string} 更新年月日
  * @property {string} 変更年月日
- * @property {string} 称号又は名称
+ * @property {string} 商号又は名称
  * @property {string} 商号又は名称イメージID
  * @property {string} 法人種別
  * @property {string} 国内所在地_都道府県
@@ -97,7 +97,7 @@ export const parsed = (csvString) => {
 
     // 2行目からがデータ
     // reference: https://www.houjin-bangou.nta.go.jp/documents/k-resource-dl.pdf
-    const dataHeader = "連番,法人番号13桁,処理区分,訂正区分,更新年月日,変更年月日,称号又は名称,商号又は名称イメージID,法人種別,国内所在地_都道府県,国内所在地_市区町村,国内所在地_丁目番地等,国内所在地_イメージID,都道府県コード,市区町村コード,郵便番号,国外所在地,国外所在地イメージID,登記記録の閉鎖等年月日,登記記録の閉鎖等の事由,承継先法人番号,変更事由の詳細,法人番号指定年月日,最新履歴,商号又は名称_英語表記,国内所在地_都道府県_英語表記,国内所在地_市区町村丁目番地等_英語表記,国外所在地_英語表記,フリガナ,検索対象除外";
+    const dataHeader = "連番,法人番号13桁,処理区分,訂正区分,更新年月日,変更年月日,商号又は名称,商号又は名称イメージID,法人種別,国内所在地_都道府県,国内所在地_市区町村,国内所在地_丁目番地等,国内所在地_イメージID,都道府県コード,市区町村コード,郵便番号,国外所在地,国外所在地イメージID,登記記録の閉鎖等年月日,登記記録の閉鎖等の事由,承継先法人番号,変更事由の詳細,法人番号指定年月日,最新履歴,商号又は名称_英語表記,国内所在地_都道府県_英語表記,国内所在地_市区町村丁目番地等_英語表記,国外所在地_英語表記,フリガナ,検索対象除外";
     const dataString = `${dataHeader}\n${csvString.split("\n").slice(1).join("\n")}`;
     /** @type {Data} */
     const data = parse(dataString, { columns: true });
