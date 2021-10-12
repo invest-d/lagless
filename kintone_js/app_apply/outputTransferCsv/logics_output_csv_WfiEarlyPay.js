9bdb4a96badc4a75c7bf9f457c5c2d1fd9db4f3c
鈴木徳 <suzuki@invest-d.com>
2021-10-12 16:26:45 +0900

    リファクタ: common_logicsに依存しないよう、util系を直接importするコードに書き換えた

--- kintone_js/app_apply/outputTransferCsv/logics_output_csv_WfiEarlyPay.js
+++ kintone_js/app_apply/outputTransferCsv/logics_output_csv_WfiEarlyPay.js
@@ -1,6 +1,36 @@
 "use strict";
 
-import * as common_logics from "./logics_output_csv";
+import { CLIENT } from "../../util/kintoneAPI";
+import { getApplyAppSchema, UnknownAppError } from "../../util/choiceApplyAppSchema";
+const applyAppSchema = (() => {
+    try {
+        return getApplyAppSchema(kintone.app.getId());
+    } catch (e) {
+        if (e instanceof UnknownAppError) {
+            alert("不明なアプリです。申込アプリで実行してください。");
+        } else {
+            console.error(e);
+            const additional_info = e.message ?? JSON.stringify(e);
+            alert("途中で処理に失敗しました。システム管理者に連絡してください。"
+                + "\n追加の情報: "
+                + `\n${additional_info}`);
+        }
+    }
+})();
+if (!applyAppSchema) throw new Error();
+const applyApp = {
+    id: applyAppSchema.id.appId,
+    fields: {
+        status: {
+            code: applyAppSchema.fields.properties.状態.code,
+        },
+        constructorId: applyAppSchema.fields.properties.constructionShopId.code,
+        paymentDate: applyAppSchema.fields.properties.paymentDate.code,
+        paymentAccount: applyAppSchema.fields.properties.paymentAccount.code,
+    },
+};
+
+export const AVAILABLE_VIEW = applyAppSchema.views.views["振込データ出力（WFI早払い）"].name;
 
 export const AVAILABLE_CONSTRUCTORS = {
     CLOSING_05: {
@@ -25,8 +55,6 @@ export const AVAILABLE_CONSTRUCTORS = {
     },
 };
 
-export const AVAILABLE_VIEW = common_logics.schema_apply.views.views["振込データ出力（WFI早払い）"].name;
-
 export const getKintoneRecords = (account, target_date, conditions) => {
     const in_query = conditions.map((c) => `"${c}"`).join(", ");
 
@@ -34,27 +62,14 @@ export const getKintoneRecords = (account, target_date, conditions) => {
 
     // 債権譲渡登記の取得が必要な申込についてもCSV出力する。GMOあおぞらの振込であれば当日着金が可能なため。
     const request_body = {
-        "app": common_logics.APP_ID_APPLY,
-        "fields": [
-            common_logics.fieldPaymentTiming_APPLY,
-            common_logics.fieldRecordId_APPLY,
-            common_logics.fieldBankCode_APPLY,
-            common_logics.fieldBranchCode_APPLY,
-            common_logics.fieldDepositType_APPLY,
-            common_logics.fieldAccountNumber_APPLY,
-            common_logics.fieldAccountName_APPLY,
-            common_logics.fieldTransferAmount_APPLY,
-            common_logics.fieldTotalReceivables_APPLY,
-            common_logics.fieldConstructionShopId_APPLY,
-            common_logics.fieldKyoryokuId_APPLY
-        ],
+        "app": applyApp.id,
         "condition": `
-                ${common_logics.fieldStatus_APPLY} in (${in_query})
-                and ${common_logics.fieldConstructionShopId_APPLY} in (${constructors})
-                and ${common_logics.fieldPaymentDate_APPLY} = "${target_date}"
-                and ${common_logics.fieldPaymentAccount_APPLY} = "${account}"
+                ${applyApp.fields.status.code} in (${in_query})
+                and ${applyApp.fields.constructorId} in (${constructors})
+                and ${applyApp.fields.paymentDate} = "${target_date}"
+                and ${applyApp.fields.paymentAccount} = "${account}"
             `
     };
 
-    return common_logics.CLIENT.record.getAllRecords(request_body);
+    return CLIENT.record.getAllRecords(request_body);
 };
