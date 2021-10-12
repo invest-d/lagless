96fb1f0fd29f277eb2990dc0cc5464e810a674cc
鈴木徳 <suzuki@invest-d.com>
2021-10-12 17:42:58 +0900

    リファクタ: rename variables

--- kintone_js/app_apply/buttonOutputTransferCsv.js
+++ kintone_js/app_apply/buttonOutputTransferCsv.js
@@ -1,20 +1,20 @@
 // @ts-check
-import * as common_logics from "./outputTransferCsv/logics_output_csv";
-import * as realtor_logics from "./outputTransferCsv/logics_output_csv_RealtorOriginalPay";
-import * as wfi_logics from "./outputTransferCsv/logics_output_csv_WfiEarlyPay";
+import * as common from "./outputTransferCsv/logics_output_csv";
+import * as realtor from "./outputTransferCsv/logics_output_csv_RealtorOriginalPay";
+import * as keban from "./outputTransferCsv/logics_output_csv_WfiEarlyPay";
 
 (function() {
     "use strict";
     // @ts-ignore
     kintone.events.on("app.record.index.show", (event) => {
-        if (common_logics.needToShow(event, "usualRealtor", realtor_logics.AVAILABLE_VIEW)) {
-            const button = common_logics.createButton({ transferType: "usualRealtor" });
+        if (common.needToShow(event, "usualRealtor", realtor.AVAILABLE_VIEW)) {
+            const button = common.createButton({ transferType: "usualRealtor" });
             // @ts-ignore
             kintone.app.getHeaderMenuSpaceElement().appendChild(button);
         }
 
-        if (common_logics.needToShow(event, "advanceKeban", wfi_logics.AVAILABLE_VIEW)) {
-            const button = common_logics.createButton({ transferType: "advanceKeban" });
+        if (common.needToShow(event, "advanceKeban", keban.AVAILABLE_VIEW)) {
+            const button = common.createButton({ transferType: "advanceKeban" });
             // @ts-ignore
             kintone.app.getHeaderMenuSpaceElement().appendChild(button);
         }
