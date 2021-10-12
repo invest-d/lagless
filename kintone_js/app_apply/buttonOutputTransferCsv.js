b7eb6234155ed020a57f1f90e576cc4d030355d8
鈴木徳 <suzuki@invest-d.com>
2021-10-12 16:51:44 +0900

    リファクタ: clickButton()の中身を全てlogics_output_csv.jsに移動

--- kintone_js/app_apply/buttonOutputTransferCsv.js
+++ kintone_js/app_apply/buttonOutputTransferCsv.js
@@ -1,19 +1,21 @@
-import * as realtor from "./button_output_csv_RealtorOriginalPay";
-import * as wfi from "./button_output_csv_WfiEarlyPay";
+// @ts-check
 import * as common_logics from "./outputTransferCsv/logics_output_csv";
 import * as realtor_logics from "./outputTransferCsv/logics_output_csv_RealtorOriginalPay";
 import * as wfi_logics from "./outputTransferCsv/logics_output_csv_WfiEarlyPay";
 
 (function() {
     "use strict";
+    // @ts-ignore
     kintone.events.on("app.record.index.show", (event) => {
-        if (common_logics.needToShow(event, realtor.transferType, realtor_logics.AVAILABLE_VIEW)) {
-            const button = realtor.createButton();
+        if (common_logics.needToShow(event, "usualRealtor", realtor_logics.AVAILABLE_VIEW)) {
+            const button = common_logics.createButton({ transferType: "usualRealtor" });
+            // @ts-ignore
             kintone.app.getHeaderMenuSpaceElement().appendChild(button);
         }
 
-        if (common_logics.needToShow(event, wfi.transferType, wfi_logics.AVAILABLE_VIEW)) {
-            const button = wfi.createButton();
+        if (common_logics.needToShow(event, "advanceKeban", wfi_logics.AVAILABLE_VIEW)) {
+            const button = common_logics.createButton({ transferType: "advanceKeban" });
+            // @ts-ignore
             kintone.app.getHeaderMenuSpaceElement().appendChild(button);
         }
     });
--- kintone_js/app_apply/outputTransferCsv/logics_output_csv.js
+++ kintone_js/app_apply/outputTransferCsv/logics_output_csv.js
@@ -1,11 +1,20 @@
+// @ts-check
 "use strict";
 
-export const CLIENT = new KintoneRestAPIClient({baseUrl: "https://investdesign.cybozu.com"});
+// @ts-ignore
+export const CLIENT = new KintoneRestAPIClient({ baseUrl: "https://investdesign.cybozu.com" });
 
 import { schema_apply as schema_apply_dev } from "../../159/schema";
 import { schema_apply as schema_apply_prod } from "../../161/schema";
 import { zenkakuToHankaku } from "../../util/characterWidth";
+import {
+    downloadFile, encodeToSjis
+} from "../../util/output_csv";
+import * as realtor from "./logics_output_csv_RealtorOriginalPay";
+import * as keban from "./logics_output_csv_WfiEarlyPay";
 
+
+// @ts-ignore
 export const APP_ID_APPLY = kintone.app.getId();
 
 export const schema_apply = ((app_id) => {
@@ -35,31 +44,68 @@ export const fieldPaymentTiming_APPLY = schema_apply.fields.properties.paymentTi
 export const fieldConstructionShopId_APPLY = schema_apply.fields.properties.constructionShopId.code;
 export const fieldKyoryokuId_APPLY = schema_apply.fields.properties.ルックアップ.code;
 
+export const getEarlyPaymentAmount = (record) => {
+    return record[fieldTransferAmount_APPLY]["value"];
+};
+
+/**
+ * @typedef {"usualRealtor"|"advanceKeban"} TransferType - リライトの通常払いか、軽バン.comの早払いにのみ対応
+*/
+
+/**
+ * @typedef {Object} ButtonParams
+ * @property {string} id
+ * @property {string} innerText
+*/
+
+/**
+ * @typedef {Object} TransferParameters
+ * @property {ButtonParams} button
+ * @property {string} targetNotation - 処理対象についての注意
+ * @property {Function} getRecords - 申込アプリから対象レコードを取得する
+ * @property {Function} getAmount - 申込アプリのレコードに対して振込金額を算出する
+ * @property {Function} getFileName - 出力するCSVファイルのファイル名を得る
+ * @property {string} completedMessage - 処理終了時に表示する文字列
+*/
+
+/**
+* @typedef {Object} Transfer
+* @property {TransferParameters} usualRealtor
+* @property {TransferParameters} advanceKeban
+*/
+
+/** @type {Transfer} */
 const TRANSFER = {
     usualRealtor: {
         button: {
             id: "outputRealtorCsv",
             innerText: "総合振込データ（リライト通常払い）",
         },
+        targetNotation: "本機能はリライト通常払い専用です。\n" + `従って、工務店が${Object.values(realtor.AVAILABLE_CONSTRUCTORS).map((c) => c.NAME).join(", ")}の申込レコードのみを対象とします。`,
+        getRecords: realtor.getKintoneRecords,
+        getAmount: realtor.getOriginalPaymentAmount,
+        getFileName: (/** @type {string} */ payment_date, /** @type {string} */ account) => `リライト通常払い振込データ（支払日：${payment_date}、振込元：${account}）.csv`,
+        completedMessage: "リライト通常払い用振込データのダウンロードを完了しました。",
     },
     advanceKeban: {
         button: {
             id: "outputWfiEarlyCsv",
             innerText: "総合振込データ（WFI早払い）",
         },
+        targetNotation: "本機能は軽バン.comの早払い専用です。\n" + `従って、工務店IDが${Object.values(keban.AVAILABLE_CONSTRUCTORS).map((c) => c.ID).join(", ")}のいずれかの申込レコードのみを対象とします。`,
+        getRecords: keban.getKintoneRecords,
+        getAmount: getEarlyPaymentAmount,
+        getFileName: (/** @type {string} */ payment_date, /** @type {string} */ account) => `軽バン.com早払い振込データ（支払日：${payment_date}、振込元：${account}）.csv`,
+        completedMessage: "軽バン.com早払い用振込データのダウンロードを完了しました。",
     },
 };
 
 /**
- * @typedef {"usualRealtor"|"advanceKeban"} TransFerType - リライトの通常払いか、軽バン.comの早払いにのみ対応
-*/
-
-/**
 * @summary クリックすると総合振込用のCSVファイルをダウンロードできるbuttonエレメントを返す
 * @param {any} event
-* @param {TransFerType} transferType
+* @param {TransferType} transferType
 * @param {string} view_name
-* @return {HTMLButtonElement}
+* @return {boolean}
 */
 export const needToShow = (event, transferType, view_name) => {
     // 一覧機能で特定の一覧を選んでいる場合のみ表示
@@ -75,14 +121,14 @@ export const needToShow = (event, transferType, view_name) => {
 /**
 * @summary クリックすると総合振込用のCSVファイルをダウンロードできるbuttonエレメントを返す
 * @param {Object} params
-* @param {TransFerType} params.transferType
-* @param {any} params.eventListener - ボタンクリック時の処理
+* @param {TransferType} params.transferType
 * @return {HTMLButtonElement}
 */
-export const createButton = ({ transferType, eventListener }) => {
+export const createButton = ({ transferType }) => {
     const button = document.createElement("button");
     button.id = TRANSFER[transferType].button.id;
     button.innerText = TRANSFER[transferType].button.innerText;
+    const eventListener = clickButtonFunc(transferType);
     button.addEventListener("click", eventListener);
     return button;
 };
@@ -92,6 +138,61 @@ export const confirmBeforeExec = () => {
         + "※このあとに支払日を指定し、\n"
         + "未出力のものだけ出力 OR 出力済みも含めて全て出力 のどちらかを選択できます。";
     return confirm(message);
+};
+
+/**
+* @param {TransferType} transferType - Brief description of the parameter here. Note: For other notations of data types, please refer to JSDocs: DataTypes command.
+* @return {Function}
+*/
+const clickButtonFunc = (transferType) => {
+    return async () => {
+        const do_download = confirmBeforeExec();
+        if (!do_download) {
+            alert("処理は中断されました。");
+            return;
+        }
+
+        const payment_date = inputPaymentDate();
+        const pattern = /^\d{4}-\d{2}-\d{2}$/;
+        if (!pattern.test(payment_date)) {
+            alert(`入力形式が正しくありませんでした。\n入力した値：${payment_date}`);
+            return;
+        }
+
+        const target_conditions = getTargetConditions();
+        alert(`${fieldStatus_APPLY}フィールドが${target_conditions.join(", ")}のレコードを対象に処理します。`);
+
+        alert(TRANSFER[transferType].targetNotation);
+
+        const account = "ID"; // GMOあおぞらから振込できるのはIDの口座のみ
+        const target_records = await TRANSFER[transferType].getRecords(account, payment_date, target_conditions)
+            .catch((err) => {
+                alert(`支払元口座：${account}のデータを取得中にエラーが発生しました。\n${err.message}`);
+                throw new Error(err);
+            });
+
+        if (target_records && target_records.length === 0) {
+            alert(`支払日：${payment_date}、振込元：${account}で、状態が${target_conditions.join(", ")}のレコードはありませんでした。`);
+            return;
+        }
+
+        try {
+            const applies_fixed_amount = target_records.map((r) => {
+                r.transfer_amount = TRANSFER[transferType].getAmount(r);
+                return r;
+            });
+            const csv_data = generateCsvData(applies_fixed_amount);
+            const sjis_list = encodeToSjis(csv_data);
+
+            downloadFile(sjis_list, TRANSFER[transferType].getFileName(payment_date, account));
+
+            await updateToDone(target_records);
+
+            alert(TRANSFER[transferType].completedMessage);
+        } catch (err) {
+            alert(err);
+        }
+    };
 };
 
 export const inputPaymentDate = () => {
@@ -122,10 +223,6 @@ export const generateCsvData = (applies) => {
     );
     // CSV文字列それぞれをCRLFで結合し、最終行の最後にもCRLFを追加して返す
     return `${csv_rows.join("\r\n")}\r\n`;
-};
-
-export const getEarlyPaymentAmount = (record) => {
-    return record[fieldTransferAmount_APPLY]["value"];
 };
 
 const getAozoraCsvRow = (record, transfer_amount) => {
