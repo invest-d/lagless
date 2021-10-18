7c0a91304715c603e7fd698155af108bb2eca2e5
鈴木徳 <suzuki@invest-d.com>
2021-10-18 12:19:54 +0900

    no message

--- kintone_js/util/choiceLaborAppSchema.js
+++ kintone_js/util/choiceLaborAppSchema.js
@@ -2,7 +2,7 @@ const ExtensibleCustomError = require("extensible-custom-error");
 export class UnknownAppError extends ExtensibleCustomError { }
 
 import { schema_181 as laborAppSchemaDev } from "../181/schema";
-import { schema_96 as laborAppSchemaProd } from "../96/schema";
+import { schema_88 as laborAppSchemaProd } from "../88/schema";
 
 /**
 * @summary 操作中アプリの開発版および本番に応じて、適切なアプリスキーマを返す
@@ -16,10 +16,10 @@ export const getLaborAppSchema = (displayingAppId) => {
 };
 
 const detectApp = (appId) => {
-    if (!appId || ![159, 161, 160, 162, 96, 181].includes(appId)) {
+    if (!appId || ![159, 161, 160, 162, 88, 181].includes(appId)) {
         throw new UnknownAppError();
     }
 
     if ([159, 160, 181].includes(appId)) return "dev";
-    if ([161, 162, 96].includes(appId)) return "prod";
+    if ([161, 162, 88].includes(appId)) return "prod";
 };
